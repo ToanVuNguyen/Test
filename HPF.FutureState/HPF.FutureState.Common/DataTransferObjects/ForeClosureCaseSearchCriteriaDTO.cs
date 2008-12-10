ff4759d3bf5a682bda56be0176c0b7c64e2cf8bd
t47nguye <t47nguye@6d9d970a-e046-46de-96ce-fbc9f6c46dd9>
2008-12-10 15:29:03 +0000

    Webservice SearchForeclosureCase
    git-svn-id: http://nt-svn-jira.vdc.csc.com/HPF/Implementation/Source Code@94 6d9d970a-e046-46de-96ce-fbc9f6c46dd9

---
+++ HPF.FutureState/HPF.FutureState.Common/DataTransferObjects/ForeClosureCaseSearchCriteriaDTO.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Practices.EnterpriseLibrary.Validation;
+using Microsoft.Practices.EnterpriseLibrary.Validation.Validators;
+using Microsoft.Practices.EnterpriseLibrary.Common.Configuration;
+
+
+namespace HPF.FutureState.Common.DataTransferObjects
+{
+    [Serializable]
+    public class ForeClosureCaseSearchCriteriaDTO
+    {
+        string _loanNumber = null;       
+        public string AgencyCaseNumber 
+        { 
+            get; 
+            set;
+            //{ if value.Equals(string.Empty)
+            //}
+        }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string LoanNumber 
+        {
+            get { return _loanNumber; }
+            set
+            {
+                _loanNumber = value;
+                
+            }
+        }
+
+        [RegexValidator("^\\d{5}$", Ruleset = "PropertyZipValidationRule")]
+        public string PropertyZip { get; set; }
+
+        [RegexValidator("^\\d{4}$", Ruleset = "SSNValidationRule")]
+        public string Last4_SSN { get; set; }
+
+    }
+}
---
+++ HPF.FutureState/HPF.FutureState.Common/DataTransferObjects/ForeClosureCaseSearchResult.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HPF.FutureState.Common.DataTransferObjects.WebServices;
+
+namespace HPF.FutureState.Common.DataTransferObjects
+{
+    public class ForeClosureCaseSearchResult:BaseDTOCollection <ForeClosureCaseWSDTO>
+    {                   
+    }
+}
