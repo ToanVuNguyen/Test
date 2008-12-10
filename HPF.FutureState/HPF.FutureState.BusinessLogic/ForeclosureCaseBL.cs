e166456f5139776d12c1f063adbd3abeef3d623c
t47nguye <t47nguye@6d9d970a-e046-46de-96ce-fbc9f6c46dd9>
2008-12-10 15:30:28 +0000

    Webservice SearchForeclosureCase
    git-svn-id: http://nt-svn-jira.vdc.csc.com/HPF/Implementation/Source Code@96 6d9d970a-e046-46de-96ce-fbc9f6c46dd9

---
+++ HPF.FutureState/HPF.FutureState.BusinessLogic/ForeclosureCaseBL.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HPF.FutureState.Common.BusinessLogicInterface;
+using HPF.FutureState.Common.DataTransferObjects;
+using HPF.FutureState.DataAccess;
+
+namespace HPF.FutureState.BusinessLogic
+{
+    public class ForeclosureCaseBL : BaseBusinessLogic, IForclosureCaseBL
+    {
+        private static readonly ForeclosureCaseBL instance = new ForeclosureCaseBL();
+        /// <summary>
+        /// Singleton
+        /// </summary>
+        public static ForeclosureCaseBL Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+        protected ForeclosureCaseBL()
+        {
+            
+        }
+
+        #region Implementation of IForclosureCaseBL
+
+        /// <summary>
+        /// Save a ForeClosureCase
+        /// </summary>
+        /// <param name="foreClosureCaseSet">ForeClosureCaseSetDTO</param>
+        public void SaveForeClosureCaseSet(ForeClosureCaseSetDTO foreClosureCaseSet)
+        {
+            //Validation here     
+       
+            var foreClosureCaseSetDAO = ForeClosureCaseSetDAO.CreateInstance();
+            //
+            try
+            {
+                foreClosureCaseSetDAO.Begin();
+                //
+                //Business process here
+                //
+                foreClosureCaseSetDAO.Commit();
+            }
+            catch (Exception)
+            {                
+                foreClosureCaseSetDAO.Cancel();
+                throw;
+            }            
+        }
+
+        public ForeClosureCaseSearchResult SearchForeClosureCase(ForeClosureCaseSearchCriteriaDTO searchCriteria)
+        {
+            return ForeClosureCaseSetDAO.CreateInstance().SearchForeClosureCase(searchCriteria);            
+        }
+        #endregion
+    }
+}
