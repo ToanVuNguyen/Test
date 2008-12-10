30f79e9cb5f586da29617abe0b2f4ab329023b6b
kdo <kdo@6d9d970a-e046-46de-96ce-fbc9f6c46dd9>
2008-12-10 11:41:45 +0000

    Unit Test web service RetrieveCallLog
    git-svn-id: http://nt-svn-jira.vdc.csc.com/HPF/Implementation/Source Code@86 6d9d970a-e046-46de-96ce-fbc9f6c46dd9

---
+++ HPF.FutureState.Webservice.Test/HPF.FutureState.WebService.Test.Web/RetrieveCallLog.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="RetrieveCallLog.aspx.cs" Inherits="HPF.FutureState.WebService.Test.Web.RetrieveCallLog" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" >
+<head runat="server">
+    <title>Untitled Page</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    <table>
+    <tr>
+    <td>
+    Call Log ID: 
+    </td>
+    <td>
+    <asp:TextBox ID="txt_CallLogId" runat="server"></asp:TextBox>
+    </td>
+    </tr>
+    <tr>
+    <td></td>
+    <td><asp:Button ID="btn_Submit" runat="server" Text="Submit" 
+            onclick="btn_Submit_Click" /></td>
+    </tr>
+    <tr>
+        <td>Status: </td>
+        <td><asp:Label ID="lbl_Status" runat="server" Text=""></asp:Label></td>
+    </tr>
+    <tr>
+        <td>Message: </td>
+        <td><asp:Label ID="lbl_Message" runat="server" Text=""></asp:Label></td>
+    </tr>
+    </table>
+        <asp:GridView ID="gv_results" runat="server">
+        </asp:GridView>
+        <asp:ObjectDataSource ID="ObjectDataSource1" runat="server">
+        </asp:ObjectDataSource>
+    </div>
+    </form>
+</body>
+</html>
---
+++ HPF.FutureState.Webservice.Test/HPF.FutureState.WebService.Test.Web/RetrieveCallLog.aspx.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+using HPF.FutureState.WebService.Test.Web.HPFCallCenterService;
+
+namespace HPF.FutureState.WebService.Test.Web
+{
+    public partial class RetrieveCallLog : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btn_Submit_Click(object sender, EventArgs e)
+        {
+            CallCenterService callCenterService = new CallCenterService();
+
+            CallLogRetrieveRequest callLogRetrieveRequest = new CallLogRetrieveRequest();
+            callLogRetrieveRequest.callLogId = txt_CallLogId.Text;
+
+            CallLogRetrieveResponse callLogRetrieveResponse = new CallLogRetrieveResponse();
+            callLogRetrieveResponse = callCenterService.RetrieveCallLog(callLogRetrieveRequest);
+                         
+            CallLogWSDTO callLogWSDTO = callLogRetrieveResponse.CallLog;           
+            
+            lbl_Status.Text = callLogRetrieveResponse.Status.ToString();
+            if (lbl_Status.Text != "Success" && lbl_Status.Text != "Warning")
+            {
+                lbl_Message.Text = callLogRetrieveResponse.Messages[0].Message.ToString();
+            }
+            if (callLogWSDTO != null)
+            {
+                ArrayList a = new ArrayList();
+                a.Add(callLogWSDTO);
+                gv_results.DataSource = a;
+                gv_results.DataBind();
+                gv_results.Visible = true;
+            }
+            else
+            {
+                gv_results.Visible = false;
+            }
+        }
+    }
+}
