90e1a06325a563281b088f971627a66c3f6e725b
t47nguye <t47nguye@6d9d970a-e046-46de-96ce-fbc9f6c46dd9>
2008-12-10 15:28:26 +0000

    Webservice SearchForeclosureCase
    git-svn-id: http://nt-svn-jira.vdc.csc.com/HPF/Implementation/Source Code@93 6d9d970a-e046-46de-96ce-fbc9f6c46dd9

--- HPF.FutureState/HPF.FutureState.DataAccess/ForeClosureCaseSetDAO.cs
+++ HPF.FutureState/HPF.FutureState.DataAccess/ForeClosureCaseSetDAO.cs
@@ -5,6 +5,7 @@ using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using HPF.FutureState.Common.DataTransferObjects;
+using HPF.FutureState.Common.DataTransferObjects.WebServices;
 using HPF.FutureState.Common.Utils.Exceptions;
 
 namespace HPF.FutureState.DataAccess
@@ -206,5 +207,82 @@ namespace HPF.FutureState.DataAccess
         {
 
         }
+
+        /// <summary>
+        /// return all search results retrieved from database
+        /// </summary>
+        /// <param name="searchCriteria"></param>
+        /// <returns></returns>
+        public ForeClosureCaseSearchResult SearchForeClosureCase(ForeClosureCaseSearchCriteriaDTO searchCriteria)
+        {           
+            ForeClosureCaseSearchResult results = null;
+
+            var dbConnection = new SqlConnection(ConnectionString);
+            var command = new SqlCommand("hpf_foreclosure_case_search", dbConnection);
+            //<Parameter>
+            var sqlParam = new SqlParameter[6];
+             	
+            sqlParam[0] = new SqlParameter("@agency_case_num", searchCriteria.AgencyCaseNumber );
+            sqlParam[1] = new SqlParameter("@borrower_fname", searchCriteria.FirstName);
+            sqlParam[2] = new SqlParameter("@borrower_lname", searchCriteria.LastName);
+            sqlParam[3] = new SqlParameter("@borrower_last4_SSN", searchCriteria.Last4_SSN);
+            sqlParam[4] = new SqlParameter("@prop_zip", searchCriteria.PropertyZip );
+            sqlParam[5] = new SqlParameter("@loan_number", searchCriteria.LoanNumber);
+            //</Parameter>
+            command.Parameters.AddRange(sqlParam);
+            command.CommandType = CommandType.StoredProcedure;
+            try
+            {
+                dbConnection.Open();
+                var reader = command.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    results = new ForeClosureCaseSearchResult();
+                    while (reader.Read())
+                    {
+                        ForeClosureCaseWSDTO item = new ForeClosureCaseWSDTO();
+                        //item.Counseled = ConvertToString(reader["counseled"]);
+                        item.FcId = ConvertToInt(reader["fc_id"]);
+                        item.IntakeDt = ConvertToDateTime(reader["intake_dt"]);
+                        item.BorrowerFname = ConvertToString(reader["borrower_fname"]);
+                        item.BorrowerLname = ConvertToString(reader["borrower_lname"]);
+                        item.BorrowerLast4SSN= ConvertToString(reader["borrower_last4_SSN"]);
+                        item.CoBorrowerFname = ConvertToString(reader["co_borrower_fname"]);
+                        item.CoBorrowerLname = ConvertToString(reader["co_borrower_lname"]);
+                        item.CoBorrowerLast4SSN = ConvertToString(reader["co_borrower_last4_SSN"]);
+                        item.PropAddr1 = ConvertToString(reader["prop_addr1"]);
+                        item.PropAddr2 = ConvertToString(reader["prop_addr2"]);
+                        item.PropCity = ConvertToString(reader["prop_city"]);
+                        item.PropStateCd = ConvertToString(reader["prop_state_cd"]);
+                        item.PropZip = ConvertToString(reader["prop_zip"]);
+                        item.AgencyName = ConvertToString(reader["agency_name"]);
+                        item.CounselorFullName = ConvertToString(reader["counselor_full_name"]);
+                        item.CounselorPhone = ConvertToString(reader["counselor_phone"]);
+                        item.CounselorExt = ConvertToString(reader["counselor_ext"]);
+                        item.CounselorEmail = ConvertToString(reader["counselor_email"]);
+                        item.CompletedDt = ConvertToDateTime(reader["completed_dt"]);
+                        //item.DelinquentDt = ConvertToDateTime(reader["Delinquent_dt"]);
+                        item.BankruptcyInd = ConvertToString(reader["bankruptcy_ind"]);
+                        item.FcNoticeReceivedInd = ConvertToString(reader["fc_notice_received_ind"]);
+                        item.LoanNumber = ConvertToString(reader["loan_number"]);
+                        item.LoanServicer = ConvertToString(reader["loan_servicer"]);
+                        item.AgencyCaseNum = ConvertToString(reader["agency_case_num"]);
+                        item.CaseLoanID = ConvertToString(reader["case_loan_id"]);
+
+
+                        results.Add(item);
+                        
+                    }
+                    reader.Close();
+                }
+                dbConnection.Close();
+            }
+            catch (Exception Ex)
+            {
+                throw ExceptionProcessor.Wrap<DataAccessException>(Ex);
+            }            
+            return results;
+        }
     }
 }
+
