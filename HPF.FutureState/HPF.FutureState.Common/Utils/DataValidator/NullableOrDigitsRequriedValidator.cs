55f983c8e2f83fda2b21cac4ce075435f8fdf378
t47nguye <t47nguye@6d9d970a-e046-46de-96ce-fbc9f6c46dd9>
2008-12-11 11:07:06 +0000

    to validate attributes in object searchCriteriaDTO
    git-svn-id: http://nt-svn-jira.vdc.csc.com/HPF/Implementation/Source Code@107 6d9d970a-e046-46de-96ce-fbc9f6c46dd9

---
+++ HPF.FutureState/HPF.FutureState.Common/Utils/DataValidator/NullableOrDigitsRequriedValidator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Text.RegularExpressions;
+
+using Microsoft.Practices.EnterpriseLibrary.Common.Configuration;
+using Microsoft.Practices.EnterpriseLibrary.Validation;
+using Microsoft.Practices.EnterpriseLibrary.Validation.Configuration;
+
+namespace HPF.FutureState.Common.Utils.DataValidator
+{
+    [ConfigurationElementType(typeof(CustomValidatorData))]
+    public class NullableOrDigitsRequriedValidator : Validator<String>
+    {
+        int _numberOfDigits;
+        bool _nullable;
+        public NullableOrDigitsRequriedValidator(bool nullable, int numberOfDigits):base(null, null)
+        {
+            _nullable = nullable;
+            _numberOfDigits = numberOfDigits;
+        }
+
+        protected override string DefaultMessageTemplate
+        {
+            get { return "Field is invalid."; }
+        }
+
+        protected override void DoValidate(string objectToValidate, object currentTarget, string key, ValidationResults validationResults)
+        {
+            bool isValid = false;
+            if (objectToValidate == null)
+                isValid = _nullable;
+            else
+            {
+                string pattern = "^\\d{" + _numberOfDigits + "}$";
+                Regex exp = new Regex(pattern);
+
+                isValid = exp.Match(objectToValidate).Success;
+            }
+            if (!isValid)
+                LogValidationResult(validationResults, MessageTemplate, currentTarget, key);
+        }
+    }
+}
---
+++ HPF.FutureState/HPF.FutureState.Common/Utils/DataValidator/NullableOrDigitsRequriedValidatorAttribute.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Practices.EnterpriseLibrary.Validation;
+using Microsoft.Practices.EnterpriseLibrary.Validation.Validators;
+
+namespace HPF.FutureState.Common.Utils.DataValidator
+{
+    public class NullableOrDigitsRequriedValidatorAttribute : ValidatorAttribute
+    {
+        int _numberOfDigits;
+        bool _nullable;
+
+        /// <summary>
+        /// Attribute for validator
+        /// </summary>
+        /// <param name="nullable">String to validate is allowed to be null or not</param>
+        /// <param name="numberOfDigits">Length of the String to validate</param>
+        public NullableOrDigitsRequriedValidatorAttribute(bool nullable, int numberOfDigits)            
+        {
+            _nullable = nullable;
+            _numberOfDigits = numberOfDigits;
+        }
+        protected override Validator DoCreateValidator(Type targetType)
+        {
+            return new NullableOrDigitsRequriedValidator(_nullable, _numberOfDigits);
+        }
+    }
+}
