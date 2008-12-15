db9b5c9328085f0391c218b23778b861d342d71b
t47nguye <t47nguye@6d9d970a-e046-46de-96ce-fbc9f6c46dd9>
2008-12-15 02:39:37 +0000

    Update InsertCallLog Web service
    git-svn-id: http://nt-svn-jira.vdc.csc.com/HPF/Implementation/Source Code@132 6d9d970a-e046-46de-96ce-fbc9f6c46dd9

---
+++ HPF.FutureState/HPF.FutureState.Common/DataTransferObjects/CallLogInsertResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using HPF.FutureState.Common.Utils.Exceptions;
+
+namespace HPF.FutureState.Common.DataTransferObjects
+{
+    public class CallLogInsertResult
+    {
+        public string CallLogID { get; set; }
+        public DataValidationException Messages { get; set; }
+    }
+}
