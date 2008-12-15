420d308a2f7f53ae660d8fd77e2bb232353eacdd
t47nguye <t47nguye@6d9d970a-e046-46de-96ce-fbc9f6c46dd9>
2008-12-15 09:14:56 +0000

    update SearchForeClosureCase function
    git-svn-id: http://nt-svn-jira.vdc.csc.com/HPF/Implementation/Source Code@139 6d9d970a-e046-46de-96ce-fbc9f6c46dd9

---
+++ HPF.FutureState/HPF.FutureState.Common/Utils/DataValidator/NullableOrPatternMatchedValidatorAttribute.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Practices.EnterpriseLibrary.Validation;
+using Microsoft.Practices.EnterpriseLibrary.Validation.Validators;
+
+namespace HPF.FutureState.Common.Utils.DataValidator
+{
+    public class NullableOrPatternMatchedValidatorAttribute : ValidatorAttribute
+    {
+        string _pattern;
+        bool _nullable;
+
+        /// <summary>
+        /// Attribute for validator
+        /// </summary>
+        /// <param name="nullable">String to validate is allowed to be null or not</param>
+        /// <param name="numberOfDigits">Length of the String to validate</param>
+        public NullableOrPatternMatchedValidatorAttribute(bool nullable, string pattern)
+        {
+            _nullable = nullable;
+            _pattern = pattern;
+        }
+        protected override Validator DoCreateValidator(Type targetType)
+        {
+            return new NullableOrPatternMatchedValidator(_nullable, _pattern);
+        }
+    }
+}
