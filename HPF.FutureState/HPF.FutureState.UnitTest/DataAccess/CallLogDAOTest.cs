128ae92f372611f471d31463f09112ef574ea894
t47nguye <t47nguye@6d9d970a-e046-46de-96ce-fbc9f6c46dd9>
2008-12-18 08:42:17 +0000

    git-svn-id: http://nt-svn-jira.vdc.csc.com/HPF/Implementation/Source Code@202 6d9d970a-e046-46de-96ce-fbc9f6c46dd9

--- HPF.FutureState/HPF.FutureState.UnitTest/DataAccess/CallLogDAOTest.cs
+++ HPF.FutureState/HPF.FutureState.UnitTest/DataAccess/CallLogDAOTest.cs
@@ -3,6 +3,12 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using HPF.FutureState.Common.DataTransferObjects;
 using HPF.FutureState.Common.DataTransferObjects.WebServices;
 
+using System;
+using System.Text;
+using System.Data.SqlClient;
+using System.Configuration;
+
+
 namespace HPF.FutureState.UnitTest
 {
     /// <summary>
@@ -121,6 +127,60 @@ namespace HPF.FutureState.UnitTest
         //    var callLogId = int.MinValue; // TODO: Initialize to an appropriate value            
         //    CallLogDTO actual = target.ReadCallLog(callLogId);
         //    Assert.AreEqual(null, actual);
-        //}                
+        //} 
+
+        #region InsertCallLog_Test
+
+        private void InsertCallLogTest_Pre()
+        {
+            var dbConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["HPFConnectionString"].ConnectionString);
+            dbConnection.Open();
+            var command = new SqlCommand();
+            command.Connection = dbConnection;
+
+            command.CommandText = "Insert into call_center(call_center_name) values ('call_center_name_1')";
+            command.ExecuteNonQuery();
+            
+            dbConnection.Close();
+
+        }
+
+        int callLogid = 0;
+        [TestMethod()]
+        public void InsertCallLogTest_Success()
+        {
+            InsertCallLogTest_Pre();
+            CallLogDAO_Accessor target = new CallLogDAO_Accessor(); // TODO: Initialize to an appropriate value           
+            
+            CallLogDTO aCallLog = new CallLogDTO();
+            aCallLog.CallCenterID = 1;            
+            int actual = target.InsertCallLog(aCallLog);
+            callLogid = actual;
+            Assert.AreNotEqual(0, actual);
+            TestContext.WriteLine("New Call Log ID: " + actual);
+
+            InsertCallLogTest_Post();
+        }
+
+        private void InsertCallLogTest_Post()
+        {
+            var dbConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["HPFConnectionString"].ConnectionString);
+            dbConnection.Open();
+            var command = new SqlCommand();
+            command.Connection = dbConnection;
+
+            command.CommandText = "Delete from Call where call_id = " + callLogid;
+            command.ExecuteNonQuery();
+
+            command.CommandText = "Delete from Call_Center where call_center_name = 'call_center_name_1'";
+            command.ExecuteNonQuery();
+
+            
+
+            dbConnection.Close();
+        }
+
+        #endregion
+
     }
 }
