4d5408be7b39f146c85de22c7f43786d9002a1ae
t47nguye <t47nguye@6d9d970a-e046-46de-96ce-fbc9f6c46dd9>
2008-12-18 04:23:16 +0000

    Exceptions for SaveForeclosureCase
    git-svn-id: http://nt-svn-jira.vdc.csc.com/HPF/Implementation/Source Code@189 6d9d970a-e046-46de-96ce-fbc9f6c46dd9

---
+++ HPF.FutureState/HPF.FutureState.Common/Utils/Exceptions/FatalException.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace HPF.FutureState.Common.Utils.Exceptions
+{
+    public class FatalException : Exception
+    {
+        public FatalException()
+        {
+            
+        }
+
+        public FatalException(string message)
+            : base(message)
+        {
+
+        }
+
+        public FatalException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+
+        }
+
+        public FatalException(string message, Exception innerException)
+            : base(message,innerException)
+        {
+
+        }
+    }
+}
---
+++ HPF.FutureState/HPF.FutureState.Common/Utils/Exceptions/ProcessingException.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace HPF.FutureState.Common.Utils.Exceptions
+{
+    public class ProcessingException:Exception
+    {
+        public ProcessingException()
+        {
+            
+        }
+
+        public ProcessingException(string message)
+            : base(message)
+        {
+
+        }
+
+        public ProcessingException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+
+        }
+
+        public ProcessingException(string message, Exception innerException)
+            : base(message,innerException)
+        {
+
+        }
+    }
+}
