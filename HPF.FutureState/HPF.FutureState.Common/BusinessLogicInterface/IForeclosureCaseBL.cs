6cb758a6be37444a3636dda907031e04598c329d
t47nguye <t47nguye@6d9d970a-e046-46de-96ce-fbc9f6c46dd9>
2008-12-18 07:16:25 +0000

    refractoring ForeClosure -> Foreclosure
    git-svn-id: http://nt-svn-jira.vdc.csc.com/HPF/Implementation/Source Code@196 6d9d970a-e046-46de-96ce-fbc9f6c46dd9

---
+++ HPF.FutureState/HPF.FutureState.Common/BusinessLogicInterface/IForeclosureCaseBL.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HPF.FutureState.Common.DataTransferObjects;
+
+namespace HPF.FutureState.Common.BusinessLogicInterface
+{
+    public interface IForeclosureCaseBL
+    {
+        /// <summary>
+        /// Save a ForeclosureCaseSet
+        /// </summary>
+        /// <param name="foreClosureCaseSet">ForeclosureCaseSetDTO</param>
+        void SaveForeclosureCaseSet(ForeclosureCaseSetDTO foreClosureCaseSet);
+
+        ForeclosureCaseSearchResult SearchForeclosureCase(ForeclosureCaseSearchCriteriaDTO searchCriteria);
+    }
+}
