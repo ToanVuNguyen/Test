2c252d2006126683b3aa0f2cedc70c252c186dda
kdo <kdo@6d9d970a-e046-46de-96ce-fbc9f6c46dd9>
2008-12-18 02:15:02 +0000

    Ref Code Item
    git-svn-id: http://nt-svn-jira.vdc.csc.com/HPF/Implementation/Source Code@181 6d9d970a-e046-46de-96ce-fbc9f6c46dd9

---
+++ HPF.FutureState/HPF.FutureState.Common/BusinessLogicInterface/RefCodeItemBL.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HPF.FutureState.Common.DataTransferObjects;
+using HPF.FutureState.Common.DataTransferObjects.WebServices;
+
+namespace HPF.FutureState.Common.BusinessLogicInterface
+{
+    public interface IRefCodeItem
+    {
+        /// <summary>
+        /// Get Ref Code Item
+        /// </summary>
+        /// <param></param>
+        /// <returns>RefCodeItemDTOCollection</returns>
+        RefCodeItemDTOCollection GetRefCodeItem();
+        /// <summary>
+       
+        
+    }
+}
