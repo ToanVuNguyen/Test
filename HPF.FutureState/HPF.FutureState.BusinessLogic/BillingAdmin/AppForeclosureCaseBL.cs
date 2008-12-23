7a2bdb364e57cdf678ac0471d14d50adbe6b472b
stran <stran@6d9d970a-e046-46de-96ce-fbc9f6c46dd9>
2008-12-23 09:50:53 +0000

    Add New
    git-svn-id: http://nt-svn-jira.vdc.csc.com/HPF/Implementation/Source Code@238 6d9d970a-e046-46de-96ce-fbc9f6c46dd9

---
+++ HPF.FutureState/HPF.FutureState.BusinessLogic/BillingAdmin/AppForeclosureCaseBL.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HPF.FutureState.Common.DataTransferObjects.BillingAdmin;
+using HPF.FutureState.DataAccess.BillingAdmin;
+
+namespace HPF.FutureState.BusinessLogic.BillingAdmin
+{
+    public class AppForeclosureCaseBL:BaseBusinessLogic
+    {
+        private static readonly AppForeclosureCaseBL _instace = new AppForeclosureCaseBL();
+        /// <summary>
+        /// Singleton
+        /// </summary>
+        public static AppForeclosureCaseBL Instance
+        {
+            get { return _instace; }
+        }
+        protected AppForeclosureCaseBL()
+        {
+
+        }
+        /// <summary>
+        /// Search Foreclosure Case
+        /// </summary>
+        /// <param name="searchCriteria">Search criteria</param>
+        /// <returns>Collection of AppForeclosureCaseSearchResult</returns>
+        public AppForeclosureCaseSearchResult AppSearchforeClosureCase(AppForeclosureCaseSearchCriteriaDTO searchCriteria)
+        {
+            AppForeclosureCaseSearchResult result = AppForeclosureCaseDAO.CreateInstance().AppSearchForeclosureCase(searchCriteria);
+            return result;
+        }
+    }
+}
