3d5fc706741119b4c265e63302c94eb2c4bfdf6c
stran <stran@6d9d970a-e046-46de-96ce-fbc9f6c46dd9>
2008-12-23 09:49:52 +0000

    Add new
    git-svn-id: http://nt-svn-jira.vdc.csc.com/HPF/Implementation/Source Code@235 6d9d970a-e046-46de-96ce-fbc9f6c46dd9

---
+++ HPF.FutureState/HPF.FutureState.Common/DataTransferObjects/BillingAdmin/AppForeclosureCaseSearchCriteriaDTO.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Text.RegularExpressions;
+
+using Microsoft.Practices.EnterpriseLibrary.Validation;
+using Microsoft.Practices.EnterpriseLibrary.Validation.Validators;
+using Microsoft.Practices.EnterpriseLibrary.Common.Configuration;
+
+using HPF.FutureState.Common.Utils.DataValidator;
+
+namespace HPF.FutureState.Common.DataTransferObjects.BillingAdmin
+{
+    [Serializable]
+    public class AppForeclosureCaseSearchCriteriaDTO
+    {
+        string _loanNumber = null;
+        //[NullableOrPatternMatchedValidator(true, "[a-zA-Z0-9]", MessageTemplate = "Agency Case ID: Only alpha-numeric characters allowed", Ruleset = "Default")]
+        public int AgencyCaseID { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+        
+        public int ForeclosureCaseID { get; set; }
+
+
+        public string LoanNumber
+        {
+            get { return _loanNumber; }
+            set
+            {
+                if (value != null)
+                {
+                    _loanNumber = value;
+                    Regex exp = new Regex(@"[^a-zA-Z0-9*]");
+                    MatchCollection matches = exp.Matches(_loanNumber);
+                    foreach (Match item in matches)
+                    {
+                        _loanNumber = _loanNumber.Replace(item.Value, string.Empty);
+                    }
+
+                    exp = new Regex(@"[^a-zA-Z0-9]");
+                    matches = exp.Matches(_loanNumber);
+                    foreach (Match item in matches)
+                    {
+                        _loanNumber = _loanNumber.Replace(item.Value, "%");
+                    }
+                }
+            }
+        }
+
+        //[NullableOrDigitsRequriedValidator(false, 5, MessageTemplate = "Property Zip: is not null and must be 5 alpha - numeric characters", Ruleset = "Default")]
+        public string PropertyZip { get; set; }
+        public string PropertyState { get; set; }
+        public int Agency { get; set; }
+        public int Program { get; set; }
+        public int Duplicates { get; set; }
+
+        //[NullableOrDigitsRequriedValidator(true, 4, MessageTemplate = "Last 4 SSN must be 4 alpha - numeric characters", Ruleset = "Default")]
+        public string Last4SSN { get; set; }
+    }
+}
---
+++ HPF.FutureState/HPF.FutureState.Common/DataTransferObjects/BillingAdmin/AppForeclosureCaseSearchResult.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HPF.FutureState.Common.DataTransferObjects.BillingAdmin
+{
+    public class AppForeclosureCaseSearchResult:BaseDTOCollection<AppForeclosureCaseSearchResult>
+    {
+        public string CaseID { get; set; }
+        public string AgencyCaseID { get; set; }
+        public string Counseled { get; set; }
+        public string CaseDate { get; set; }
+        public string BorrowerFirstName { get; set; }
+        public string BorrowerLastName { get; set; }
+        public string Last4SSN { get; set; }
+        public string CoborrowerFirstName { get; set; }
+        public string CoborrowerLastName { get; set; }
+        public string CoborrowerLast4SSN { get; set; }
+        public string PropertyAddress { get; set; }
+        public string PropertyCity { get; set; }
+        public string PropertyState { get; set; }
+        public string PropertyZip { get; set; }
+        public string AgencyName { get; set; }
+        public string AgentName { get; set; }
+        public string AgentPhone { get; set; }
+        public string AgentExtension { get; set; }
+        public string AgentEmail { get; set; }
+        public string CaseCompleteDate { get; set; }
+        public string DaysDelinquent { get; set; }
+        public string BankruptcyIndicator { get; set; }
+        public string ForeclosureNoticeReceivedIndicator { get; set; }
+        public string LoanList { get; set; }
+    }
+}
