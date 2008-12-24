10a26203ffe050d03844927a1371b1aefda3bbae
t55nguye <t55nguye@6d9d970a-e046-46de-96ce-fbc9f6c46dd9>
2008-12-24 06:39:02 +0000

    git-svn-id: http://nt-svn-jira.vdc.csc.com/HPF/Implementation/Source Code@290 6d9d970a-e046-46de-96ce-fbc9f6c46dd9

---
+++ HPF.FutureState/HPF.FutureState.Web/BillingAdmin/AppForeClosureCaseSearch.ascx
@@ -0,0 +1,155 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="AppForeClosureCaseSearch.ascx.cs" Inherits="HPF.FutureState.Web.BillingAdmin.AppForeClosureCaseSearch" %>
+<%@ Register Assembly="Microsoft.Practices.EnterpriseLibrary.Validation.Integration.AspNet"
+    Namespace="Microsoft.Practices.EnterpriseLibrary.Validation.Integration.AspNet"
+    TagPrefix="cc1" %>
+<%@ Register Src="~/BillingAdmin/FixedHeaderGrid.ascx" TagName="FixedHeaderGrid"  TagPrefix="uc1" %>
+<style type="text/css">
+    .style1
+    {
+        width: 100%;
+        
+    }
+    .title 
+    {
+            font-weight:bold;
+            text-align:right;
+    }
+    .style2
+    {
+        color: #0066FF;
+        font-weight: bold;
+        font-size: medium;
+        text-align:center;
+    }
+</style>
+<table class="style1">
+    <colgroup>
+    <col=30% />
+    <col=30%" />
+    <col=60% />
+    </colgroup>
+    <tr style=" ">
+        <td colspan="6" class="style2">
+            Foreclosure Case Search</td>
+    </tr>
+    <tr>
+        <td colspan="6">
+            Search Criteria:</td>
+    </tr>
+    <tr>
+        <td class="title">
+            Last 4 or SSN:
+        </td>
+        <td>
+            <asp:TextBox ID="txtSSN" runat="server"></asp:TextBox>
+        </td>
+        <td class="title">
+            Agency Case ID:</td>
+        <td>
+            <asp:TextBox ID="txtAgencyCaseID" runat="server"></asp:TextBox>
+        </td>
+        <td class="title">
+            Duplicate:
+        </td>
+        <td>
+            <asp:DropDownList ID="ddlDup" runat="server">
+                <asp:ListItem Value='Y'>Y</asp:ListItem>
+                <asp:ListItem Value='N'>N</asp:ListItem>
+                <asp:ListItem Selected="True" Value=''></asp:ListItem>
+            </asp:DropDownList>
+        </td>
+    </tr>
+    <tr>
+        <td class="title">
+            Last Name:         </td>
+        <td>
+            <asp:TextBox ID="txtLastName" runat="server"></asp:TextBox>
+        </td>
+        <td class="title">
+            Loan Number:
+        </td>
+        <td>
+            <asp:TextBox ID="txtLoanNum" runat="server"></asp:TextBox>
+        </td>
+        <td class="title">
+            Agency:</td>
+        <td>
+            <asp:DropDownList ID="ddlAgency" runat="server" Width="100px">
+            </asp:DropDownList>
+            
+        </td>
+    </tr>
+    <tr>
+        <td class="title">
+            First Name:         </td>
+        <td>
+            <asp:TextBox ID="txtFirstName" runat="server"></asp:TextBox>
+            <cc1:PropertyProxyValidator ID="txtFirstNameCheckValidator" runat="server" PropertyName="FirstName" RulesetName="Default" ControlToValidate="txtFirstName" SourceTypeName="HPF.FutureState.Common.DataTransferObjects.BillingAdmin.AppForeclosureCaseSearchCriteriaDTO"></cc1:PropertyProxyValidator>
+        </td>
+        <td class="title">
+            Property Zip:</td>
+        <td>
+            <asp:TextBox ID="txtPropertyZip" runat="server"></asp:TextBox>
+            <%--<cc1:PropertyProxyValidator ID="txtPropertyZipCheckValidator" runat="server" PropertyName="PropertyZip" RulesetName="Default" ControlToValidate="txtPropertyZip" SourceTypeName="HPF.FutureState.Common.DataTransferObjects.BillingAdmin.AppForeclosureCaseSearchCriteriaDTO"></cc1:PropertyProxyValidator>--%>
+        </td>
+        <td class="title">
+            Program:</td>
+        <td>
+            <asp:DropDownList ID="ddlProgram" runat="server" Width="100px">
+            </asp:DropDownList>
+        </td>
+    </tr>
+    <tr>
+        <td class="title">
+            Foreclosure Case ID:
+            </td>
+        <td>
+            <asp:TextBox ID="txtForeclosureCaseID" runat="server"></asp:TextBox>
+            <cc1:PropertyProxyValidator ID="txtForecloseureCaseIDCheckValidate" runat="server" PropertyName="ForeclosureCaseID" RulesetName="Default" ControlToValidate="txtForeclosureCaseID" SourceTypeName="HPF.FutureState.Common.DataTransferObjects.BillingAdmin.AppForeclosureCaseSearchCriteriaDTO"></cc1:PropertyProxyValidator>
+        </td>
+        <td class="title">
+            Property State:</td>
+        <td>
+            <asp:DropDownList ID="ddlPropertyState" runat="server" Width="100px">
+            </asp:DropDownList>
+        </td>
+        <td>
+            &nbsp;</td>
+        <td>
+            <asp:Button ID="btnSearch" runat="server" Text="Search" Width="100px" />
+        </td>
+    </tr>
+    <tr>
+        <td>
+            &nbsp;</td>
+        <td>
+            &nbsp;</td>
+        <td>
+            &nbsp;</td>
+        <td>
+            &nbsp;</td>
+        <td>
+            &nbsp;</td>
+        <td>
+            &nbsp;</td>
+    </tr>
+    <tr>
+        <td colspan="6">
+            <uc1:FixedHeaderGrid ID="grvForeClosureCaseSearch" runat="server" />
+        </td>
+    </tr>
+    <tr>
+        <td>
+            &nbsp;</td>
+        <td>
+            &nbsp;</td>
+        <td>
+            &nbsp;</td>
+        <td>
+            &nbsp;</td>
+        <td>
+            &nbsp;</td>
+        <td>
+            &nbsp;</td>
+    </tr>
+</table>
---
+++ HPF.FutureState/HPF.FutureState.Web/BillingAdmin/AppForeClosureCaseSearch.ascx.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+using System.Collections.Generic;
+//using HPF.FutureState.BusinessLogic.BillingAdmin;
+//using HPF.FutureState.Common.DataTransferObjects.BillingAdmin;
+
+namespace HPF.FutureState.Web.BillingAdmin
+
+{
+
+    public partial class AppForeClosureCaseSearch : System.Web.UI.UserControl
+    {
+        
+        protected AppForeclosureCaseSearchCriteriaDTO appForeclosureCaseSearchCriteriaDTO = new AppForeclosureCaseSearchCriteriaDTO();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+          
+            BindDdlProgram();
+            BindDdlState();
+            BindDdlAgency();
+            BindGrvForeClosureCaseSearch();
+
+        }
+        protected void BindDdlState()
+        {
+            //DataSet dsProgram = AppForeclosureCaseBL.Instance.GetState();
+            //string ValueField = dsProgram.Tables[0].Columns["prop_state_cd"].ToString();
+            //string TextField = dsProgram.Tables[0].Columns["prop_state_cd"].ToString();
+            //BindDll(dsProgram, ddlPropertyState, ValueField, TextField, "", "All");
+        }
+        protected void BindDdlAgency()
+        {
+            //DataSet dsProgram = AppForeclosureCaseBL.Instance.GetAgency();
+            //string ValueField = dsProgram.Tables[0].Columns["agency_id"].ToString();
+            //string TextField = dsProgram.Tables[0].Columns["agency_name"].ToString();
+            //BindDll(dsProgram, ddlAgency, ValueField, TextField, "", "");
+        }
+        protected void BindDdlProgram()
+        {
+            //DataSet dsProgram = AppForeclosureCaseBL.Instance.GetProgram();
+            //string ValueField = dsProgram.Tables[0].Columns["program_id"].ToString();
+            //string TextField = dsProgram.Tables[0].Columns["program_name"].ToString();
+            //BindDll(dsProgram,ddlProgram,ValueField,TextField,"-1","ALL");
+        }
+
+        protected void BindGrvForeClosureCaseSearch()
+        {
+            //grvForeClosureCaseSearch.ColumsName = new List<string> { "Case ID", "Agency Case ID", "Counseled", "Case Date", "Borrower Name", "SSN", "Co_Borrower", "SSN", "Property Address", "Property City", "Property State", "PropertyZip", "Agency Name","Agent Name","Agent Phone","Agent Extension","Agent Extension","Agent Email","Case Complete Date","Days Deliquent","Bankruptcy Indicator","Foreclosre Notice Received Indicator","Loan List" };
+            //grvForeClosureCaseSearch.ColumsWidth = new List<int> { 100, 300, 300, 300, 300, 300, 300, 300, 300, 300, 300, 300, 300, 100, 300, 300, 300, 300, 300, 300, 300, 300, 300};
+            //grvForeClosureCaseSearch.HeaderCssClass = "header";
+
+            //appForeclosureCaseSearchCriteriaDTO.Last4SSN=txtSSN.Text==string.Empty?null:txtSSN.Text;
+            //appForeclosureCaseSearchCriteriaDTO.LastName =  txtLastName.Text==string.Empty?null:txtLastName.Text;
+            //appForeclosureCaseSearchCriteriaDTO.FirstName = txtFirstName.Text==string.Empty?null:txtFirstName.Text;
+            //appForeclosureCaseSearchCriteriaDTO.ForeclosureCaseID = 676996;
+            //    //txtForeclosureCaseID.Text==""?-1:int.Parse(txtForeclosureCaseID.Text);
+            //appForeclosureCaseSearchCriteriaDTO.AgencyCaseID =txtAgencyCaseID.Text==""?-1:int.Parse(txtAgencyCaseID.Text);
+            //appForeclosureCaseSearchCriteriaDTO.LoanNumber = txtLoanNum.Text==string.Empty?null:txtLoanNum.Text;
+            //appForeclosureCaseSearchCriteriaDTO.PropertyZip = txtPropertyZip.Text==string.Empty?null:txtPropertyZip.Text;
+            //appForeclosureCaseSearchCriteriaDTO.PropertyState = ddlPropertyState.SelectedValue == string.Empty ? null : ddlPropertyState.SelectedValue;
+            //appForeclosureCaseSearchCriteriaDTO.Duplicates = ddlDup.SelectedValue.ToString() == string.Empty ? null : ddlDup.SelectedValue.ToString();
+            //appForeclosureCaseSearchCriteriaDTO.Agency = int.Parse(ddlAgency.SelectedValue);
+            //appForeclosureCaseSearchCriteriaDTO.Program = int.Parse(ddlProgram.SelectedValue);
+            //var temp = AppForeclosureCaseBL.Instance.AppSearchforeClosureCase(appForeclosureCaseSearchCriteriaDTO);
+            //grvForeClosureCaseSearch.DataSource = temp;
+            
+
+            
+        }
+        protected void BindDll(DataSet ds, DropDownList ddl,string ValueField,string TextField,string InitValue,string InitText)
+        {
+            ddl.DataSource = ds;
+            ddl.DataTextField = TextField;
+            ddl.DataValueField = ValueField;
+            ddl.DataBind();
+            if (InitText != "" )
+            {
+                ListItem item = new ListItem(InitText, InitValue);
+                ddl.Items.Add(item);
+            }
+            if (InitText == "") InitText = "ALL";
+            ddl.Items.FindByText(InitText).Selected = true;
+        }
+
+
+        
+    }
+}
