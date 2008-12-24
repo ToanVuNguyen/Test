5437dee06431c547dbe13865a62f4848a410abee
t47nguye <t47nguye@6d9d970a-e046-46de-96ce-fbc9f6c46dd9>
2008-12-24 04:02:24 +0000

    remove constructor
    git-svn-id: http://nt-svn-jira.vdc.csc.com/HPF/Implementation/Source Code@270 6d9d970a-e046-46de-96ce-fbc9f6c46dd9

--- HPF.FutureState/HPF.FutureState.Common/DataTransferObjects/CallLogDTO.cs
+++ HPF.FutureState/HPF.FutureState.Common/DataTransferObjects/CallLogDTO.cs
@@ -103,104 +103,45 @@ namespace HPF.FutureState.Common.DataTransferObjects
 
         #endregion
 
-        
+        #region rem
 
+        //public CallLogDTO(CallLogWSDTO callLog)
+        //{
+        //    if (callLog.CallId != null)
+        //    {
+        //        callLog.CallId = callLog.CallId.Replace("HPF_", "");
+        //        int id = 0;
+        //        int.TryParse(callLog.CallId, out id);
+        //        this.CallId = id;
+        //    }
+        //    this.CallCenterID = callLog.CallCenterID;
+        //    this.CcAgentIdKey = callLog.CcAgentIdKey;
+        //    this.StartDate = callLog.StartDate;
+        //    this.EndDate = callLog.EndDate;
+        //    this.DNIS = callLog.DNIS;
+        //    this.CallCenter = callLog.CallCenter;            
+        //    this.CallSourceCd = callLog.CallSourceCd;
+        //    this.ReasonToCall = callLog.ReasonToCall;
+        //    this.LoanAccountNumber = callLog.LoanAccountNumber;
+        //    this.FirstName = callLog.FirstName;
+        //    this.LastName = callLog.LastName;              
+        //    this.ServicerId = callLog.ServicerId;
+        //    this.OtherServicerName = callLog.OtherServicerName;
+        //    this.PropZipFull9 = callLog.PropZipFull9;            
+        //    this.PrevAgencyId = callLog.PrevAgencyId;
+        //    this.SelectedAgencyId = callLog.SelectedAgencyId;
+        //    this.ScreenRout = callLog.ScreenRout;
+        //    this.FinalDispoCd = callLog.FinalDispoCd;
+        //    this.TransNumber = callLog.TransNumber;            
+        //    this.CreateDate = callLog.CreateDate;
+        //    this.CreateUserId = callLog.CreateUserId;
+        //    this.CreateAppName = callLog.CreateAppName;
+        //    this.ChangeLastDate = callLog.ChangeLastDate;
+        //    this.ChangeLastUserId = callLog.ChangeLastUserId;
+        //    this.ChangeLastAppName = callLog.ChangeLastAppName;
+        //}
 
-        #region REM
-
-        //public int CallId { get; set; }
-        //[StringRequiredValidator(Ruleset = "Default", MessageTemplate = "External call number is required.")]
-        //public string ExtCallNumber { get; set; }
-
-        //public string AgencyId { get; set; }
-
-        //[InTakeDateValidator(60, MessageTemplate = "StartDate problem", Ruleset = "Default")]
-        //public DateTime StartDate { get; set; }
-        
-        //public DateTime EndDate { get; set; }
-
-        //public string DNIS { get; set; }
-
-        //public string CallCenter { get; set; }
-
-        //public string CallCenterCD { get; set; }
-
-        //public string CallResource { get; set; }
-
-        //public string ReasonToCall { get; set; }
-
-        //public string AccountNumber { get; set; }
-
-        //public string FirstName { get; set; }
-
-        //public string LastName { get; set; }
-
-        //public string CounselPastYRInd { get; set; }
-
-        //public string MtgProbInd { get; set; }
-
-        //public string PastDueInd { get; set; }
-
-        //public string PastDueSoonInd { get; set; }
-
-        //public int PastDueMonths { get; set; }
-
-        //public int ServicerId { get; set; }
-
-        //public string OtherServicerName { get; set; }
-
-        //public string PropZip { get; set; }
-
-        //public int PrevCounselorId { get; set; }
-
-        //public int PrevAgencyId { get; set; }
-
-        //public string SelectedAgencyId { get; set; }       
-
-        //public string ScreenRout { get; set; }
-
-        //public int FinalDispo { get; set; }
-
-        //public string TransNumber { get; set; }
-
-        //public string OutOfNetworkReferralTBD { get; set; }
+        //public CallLogDTO() { }
         #endregion
-        public CallLogDTO(CallLogWSDTO callLog)
-        {
-            if (callLog.CallId != null)
-            {
-                callLog.CallId = callLog.CallId.Replace("HPF_", "");
-                int id = 0;
-                int.TryParse(callLog.CallId, out id);
-                this.CallId = id;
-            }
-            this.CallCenterID = callLog.CallCenterID;
-            this.CcAgentIdKey = callLog.CcAgentIdKey;
-            this.StartDate = callLog.StartDate;
-            this.EndDate = callLog.EndDate;
-            this.DNIS = callLog.DNIS;
-            this.CallCenter = callLog.CallCenter;            
-            this.CallSourceCd = callLog.CallSourceCd;
-            this.ReasonToCall = callLog.ReasonToCall;
-            this.LoanAccountNumber = callLog.LoanAccountNumber;
-            this.FirstName = callLog.FirstName;
-            this.LastName = callLog.LastName;              
-            this.ServicerId = callLog.ServicerId;
-            this.OtherServicerName = callLog.OtherServicerName;
-            this.PropZipFull9 = callLog.PropZipFull9;            
-            this.PrevAgencyId = callLog.PrevAgencyId;
-            this.SelectedAgencyId = callLog.SelectedAgencyId;
-            this.ScreenRout = callLog.ScreenRout;
-            this.FinalDispoCd = callLog.FinalDispoCd;
-            this.TransNumber = callLog.TransNumber;            
-            this.CreateDate = callLog.CreateDate;
-            this.CreateUserId = callLog.CreateUserId;
-            this.CreateAppName = callLog.CreateAppName;
-            this.ChangeLastDate = callLog.ChangeLastDate;
-            this.ChangeLastUserId = callLog.ChangeLastUserId;
-            this.ChangeLastAppName = callLog.ChangeLastAppName;
-        }
-
-        public CallLogDTO() { }
     }
 }
