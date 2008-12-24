6289c3f0c44e2b1283d0502197d0330dbf23955f
t47nguye <t47nguye@6d9d970a-e046-46de-96ce-fbc9f6c46dd9>
2008-12-24 02:06:19 +0000

    multiple instances of DAO remove SearchForeclosureCase to ForeclosureCaseDAO
    git-svn-id: http://nt-svn-jira.vdc.csc.com/HPF/Implementation/Source Code@253 6d9d970a-e046-46de-96ce-fbc9f6c46dd9

--- HPF.FutureState/HPF.FutureState.DataAccess/ForeclosureCaseSetDAO.cs
+++ HPF.FutureState/HPF.FutureState.DataAccess/ForeclosureCaseSetDAO.cs
@@ -18,7 +18,6 @@ namespace HPF.FutureState.DataAccess
 
         public SqlTransaction trans;
 
-        private static ForeclosureCaseSetDAO _instance = new ForeclosureCaseSetDAO();
 
         protected ForeclosureCaseSetDAO()
         {
@@ -27,8 +26,7 @@ namespace HPF.FutureState.DataAccess
 
         public static ForeclosureCaseSetDAO CreateInstance()
         {
-            return _instance;
-            //return new ForeclosureCaseSetDAO();
+            return new ForeclosureCaseSetDAO();
         }
 
         /// <summary>
@@ -812,80 +810,6 @@ namespace HPF.FutureState.DataAccess
             {
                 throw ExceptionProcessor.Wrap<DataAccessException>(Ex);
             }
-        }
-
-        /// <summary>
-        /// return all search results retrieved from database
-        /// </summary>
-        /// <param name="searchCriteria"></param>
-        /// <returns></returns>
-        public ForeclosureCaseSearchResult SearchForeclosureCase(ForeclosureCaseSearchCriteriaDTO searchCriteria)
-        {
-            ForeclosureCaseSearchResult results = new ForeclosureCaseSearchResult();
-
-            var dbConnection = new SqlConnection(ConnectionString);
-            var command = new SqlCommand("hpf_foreclosure_case_search", dbConnection);
-            //<Parameter>
-            var sqlParam = new SqlParameter[6];
-
-            sqlParam[0] = new SqlParameter("@pi_agency_case_num", searchCriteria.AgencyCaseNumber);
-            sqlParam[1] = new SqlParameter("@pi_borrower_fname", searchCriteria.FirstName);
-            sqlParam[2] = new SqlParameter("@pi_borrower_lname", searchCriteria.LastName);
-            sqlParam[3] = new SqlParameter("@pi_borrower_last4_SSN", searchCriteria.Last4_SSN);
-            sqlParam[4] = new SqlParameter("@pi_prop_zip", searchCriteria.PropertyZip);
-            sqlParam[5] = new SqlParameter("@pi_loan_number", searchCriteria.LoanNumber);
-            //</Parameter>
-            command.Parameters.AddRange(sqlParam);
-            command.CommandType = CommandType.StoredProcedure;
-            try
-            {
-                dbConnection.Open();
-                var reader = command.ExecuteReader();
-                if (reader.HasRows)
-                {
-                    results = new ForeclosureCaseSearchResult();
-                    while (reader.Read())
-                    {
-                        ForeclosureCaseWSDTO item = new ForeclosureCaseWSDTO();
-                        //item.Counseled = ConvertToString(reader["counseled"]);
-                        item.FcId = ConvertToInt(reader["fc_id"]);
-                        item.IntakeDt = ConvertToDateTime(reader["intake_dt"]);
-                        item.BorrowerFname = ConvertToString(reader["borrower_fname"]);
-                        item.BorrowerLname = ConvertToString(reader["borrower_lname"]);
-                        item.BorrowerLast4SSN = ConvertToString(reader["borrower_last4_SSN"]);
-                        item.CoBorrowerFname = ConvertToString(reader["co_borrower_fname"]);
-                        item.CoBorrowerLname = ConvertToString(reader["co_borrower_lname"]);
-                        item.CoBorrowerLast4SSN = ConvertToString(reader["co_borrower_last4_SSN"]);
-                        item.PropAddr1 = ConvertToString(reader["prop_addr1"]);
-                        item.PropAddr2 = ConvertToString(reader["prop_addr2"]);
-                        item.PropCity = ConvertToString(reader["prop_city"]);
-                        item.PropStateCd = ConvertToString(reader["prop_state_cd"]);
-                        item.PropZip = ConvertToString(reader["prop_zip"]);
-                        item.AgencyName = ConvertToString(reader["agency_name"]);
-                        item.CounselorFullName = ConvertToString(reader["counselor_full_name"]);
-                        item.CounselorPhone = ConvertToString(reader["counselor_phone"]);
-                        item.CounselorExt = ConvertToString(reader["counselor_ext"]);
-                        item.CounselorEmail = ConvertToString(reader["counselor_email"]);
-                        item.CompletedDt = ConvertToDateTime(reader["completed_dt"]);
-                        //item.DelinquentDt = ConvertToDateTime(reader["Delinquent_dt"]);
-                        item.BankruptcyInd = ConvertToString(reader["bankruptcy_ind"]);
-                        item.FcNoticeReceivedInd = ConvertToString(reader["fc_notice_received_ind"]);
-                        item.LoanNumber = ConvertToString(reader["loan_number"]);
-                        item.LoanServicer = ConvertToString(reader["loan_servicer"]);
-                        item.AgencyCaseNum = ConvertToString(reader["agency_case_num"]);
-                        item.CaseLoanID = ConvertToString(reader["case_loan_id"]);
-                        results.Add(item);
-
-                    }
-                    reader.Close();
-                }
-                dbConnection.Close();
-            }
-            catch (Exception Ex)
-            {
-                throw ExceptionProcessor.Wrap<DataAccessException>(Ex);
-            }
-            return results;
         }
 
         public bool CheckExistingAgencyIdAndCaseNumber(int agency_id, string agency_case_number)
