2033c94bd45f200dc6669d95046f218fd2c36ddb
t47nguye <t47nguye@6d9d970a-e046-46de-96ce-fbc9f6c46dd9>
2008-12-24 02:02:32 +0000

    search ForeclosureCase
    git-svn-id: http://nt-svn-jira.vdc.csc.com/HPF/Implementation/Source Code@249 6d9d970a-e046-46de-96ce-fbc9f6c46dd9

---
+++ HPF.FutureState/HPF.FutureState.Common/DataTransferObjects/WebServices/ForeclosureCaseSearchResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HPF.FutureState.Common.DataTransferObjects.WebServices
+{
+    public class ForeclosureCaseSearchResponse:BaseResponse
+    {
+        public ForeclosureCaseSearchResult Results { get; set; }
+        public int SearchResultCount { get; set; }       
+    }
+}
