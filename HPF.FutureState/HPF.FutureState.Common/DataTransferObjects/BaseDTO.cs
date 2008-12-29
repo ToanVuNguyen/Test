e5ef3627e902549585341f8080e3ba98706093f9
ltcao <ltcao@6d9d970a-e046-46de-96ce-fbc9f6c46dd9>
2008-12-29 02:20:00 +0000

    Ignore all properties in BaseDTO from Web service consumer.
    git-svn-id: http://nt-svn-jira.vdc.csc.com/HPF/Implementation/Source Code@327 6d9d970a-e046-46de-96ce-fbc9f6c46dd9

--- HPF.FutureState/HPF.FutureState.Common/DataTransferObjects/BaseDTO.cs
+++ HPF.FutureState/HPF.FutureState.Common/DataTransferObjects/BaseDTO.cs
@@ -9,17 +9,22 @@ namespace HPF.FutureState.Common.DataTransferObjects
     [Serializable]
     public class BaseDTO
     {
-        
+        [XmlIgnore]
         public DateTime CreateDate { get; set; }
 
+        [XmlIgnore]
         public string CreateUserId { get; set; }
 
+        [XmlIgnore]
         public string CreateAppName { get; set; }
 
+        [XmlIgnore]
         public DateTime ChangeLastDate { get; set; }
 
+        [XmlIgnore]
         public string ChangeLastUserId { get; set; }
 
+        [XmlIgnore]
         public string ChangeLastAppName { get; set; }
     }
 }
