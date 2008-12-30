645847422db585726ee92e643927f3ebf1354d9f
t47nguye <t47nguye@6d9d970a-e046-46de-96ce-fbc9f6c46dd9>
2008-12-30 02:22:03 +0000

    Update ProcessingException Update ErrorMessages
    git-svn-id: http://nt-svn-jira.vdc.csc.com/HPF/Implementation/Source Code@395 6d9d970a-e046-46de-96ce-fbc9f6c46dd9

--- HPF.FutureState/HPF.FutureState.Common/ErrorMessages.cs
+++ HPF.FutureState/HPF.FutureState.Common/ErrorMessages.cs
@@ -9,6 +9,15 @@ namespace HPF.FutureState.Common
     {
         public const string AUTHENTICATION_ERROR_MSG = "Authentication failed, access to web service is denied";
 
+        public const string PROCESSING_EXCEPTION_NULL_FORECLOSURE_CASE_SET = "Foreclosure case set is null";
+        public const string PROCESSING_EXCEPTION_NULL_FORECLOSURE_CASE = "Foreclosure case is null";       
+        public const string PROCESSING_EXCEPTION_DUPLICATE_FORECLOSURE_CASE = "Duplicate Foreclosure case";        
+        public const string PROCESSING_EXCEPTION_INVALID_AGENCY_CASE_NUM_OR_AGENCY_ID= "Either Agency Case Number or Agency ID is invalid";
+        public const string PROCESSING_EXCEPTION_EXISTING_AGENCY_CASE_NUM_AND_AGENCY_ID = "Case with pair of Agency Case Number and Agency ID already existed";
+        public const string PROCESSING_EXCEPTION_INVALID_FC_ID = "Invalid Foreclosure Case ID";
+        public const string PROCESSING_EXCEPTION_INVALID_FC_ID_FOR_AGENCY_ID = "Case with pair of Agency Case Number and Agency ID is not in Database";
+        public const string MISC_PROCESSING_EXCEPTION = "Miscellaneous processing exception";
+
         private static readonly Dictionary<int, string> errorMessageDict = new Dictionary<int, string>();
 
         public static string GetExceptionMessage(int exceptionId)
--- HPF.FutureState/HPF.FutureState.Common/Utils/Exceptions/ProcessingException.cs
+++ HPF.FutureState/HPF.FutureState.Common/Utils/Exceptions/ProcessingException.cs
@@ -8,27 +8,33 @@ namespace HPF.FutureState.Common.Utils.Exceptions
 {
     public class ProcessingException:Exception
     {
+        public ExceptionMessageCollection ExceptionMessages { get; set; }
+
         public ProcessingException()
         {
-            
+            Init();
         }
 
         public ProcessingException(string message)
             : base(message)
         {
-
+            Init();
         }
 
         public ProcessingException(SerializationInfo info, StreamingContext context)
             : base(info, context)
         {
-
+            Init();
         }
 
         public ProcessingException(string message, Exception innerException)
             : base(message,innerException)
         {
-
+            Init();
+        }
+        private void Init()
+        {
+            ExceptionMessages = new ExceptionMessageCollection();
         }
     }
 }
