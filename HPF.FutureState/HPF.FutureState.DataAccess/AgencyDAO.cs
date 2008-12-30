bbd90e9e109ed3598280c23f1c5e5d319b5879aa
t47nguye <t47nguye@6d9d970a-e046-46de-96ce-fbc9f6c46dd9>
2008-12-30 09:32:58 +0000

    git-svn-id: http://nt-svn-jira.vdc.csc.com/HPF/Implementation/Source Code@423 6d9d970a-e046-46de-96ce-fbc9f6c46dd9

---
+++ HPF.FutureState/HPF.FutureState.DataAccess/AgencyDAO.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using HPF.FutureState.Common.DataTransferObjects;
+using HPF.FutureState.Common.DataTransferObjects.WebServices;
+using HPF.FutureState.Common.Utils.Exceptions;
+
+namespace HPF.FutureState.DataAccess
+{
+    public class AgencyDAO:BaseDAO
+    {
+        private static readonly AgencyDAO instance = new AgencyDAO();
+        /// <summary>
+        /// Singleton
+        /// </summary>
+        public static AgencyDAO Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+        protected AgencyDAO()
+        {
+            
+        }
+
+        public string GetAgencyName(int AgencyID)
+        {
+            string returnString;
+            SqlConnection dbConnection = base.CreateConnection();
+            try
+            {
+                SqlCommand command = base.CreateCommand("hpf_agency_get_from_agency_id", dbConnection);
+                //<Parameter>
+                SqlParameter[] sqlParam = new SqlParameter[1];
+                sqlParam[0] = new SqlParameter("@pi_agency_id", AgencyID);
+
+                //</Parameter>
+                command.Parameters.AddRange(sqlParam);
+                command.CommandType = CommandType.StoredProcedure;
+
+                dbConnection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    if (reader.Read())
+                    {
+                        returnString = ConvertToString(reader["agency_name"]);
+                    }
+                    reader.Close();
+                }
+                else
+                    returnString = string.Empty;
+
+            }
+            catch (Exception Ex)
+            {
+                throw ExceptionProcessor.Wrap<DataAccessException>(Ex);
+            }
+            finally
+            {
+                dbConnection.Close();
+            }
+            return returnString;
+        }
+    }
+}
