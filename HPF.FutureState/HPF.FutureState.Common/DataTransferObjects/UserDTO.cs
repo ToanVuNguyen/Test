fde664dda245dbdcd3c7f43a526a576d50ca3ece
ltcao <ltcao@6d9d970a-e046-46de-96ce-fbc9f6c46dd9>
2008-12-31 02:14:07 +0000

    Create web user DTO
    git-svn-id: http://nt-svn-jira.vdc.csc.com/HPF/Implementation/Source Code@439 6d9d970a-e046-46de-96ce-fbc9f6c46dd9

---
+++ HPF.FutureState/HPF.FutureState.Common/DataTransferObjects/UserDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HPF.FutureState.Common.DataTransferObjects
+{
+    public class UserDTO : BaseDTO
+    {        
+
+        public string UserLoginId { get; set; }
+
+        public string UserRole { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+    }
+}
