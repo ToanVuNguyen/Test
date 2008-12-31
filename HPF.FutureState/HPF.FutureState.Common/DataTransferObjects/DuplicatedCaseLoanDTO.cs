59bbd3e4ba2488370b64c4c7d31d9e62c8d76e85
t47nguye <t47nguye@6d9d970a-e046-46de-96ce-fbc9f6c46dd9>
2008-12-31 02:58:56 +0000

    update ProcessingException
    git-svn-id: http://nt-svn-jira.vdc.csc.com/HPF/Implementation/Source Code@442 6d9d970a-e046-46de-96ce-fbc9f6c46dd9

---
+++ HPF.FutureState/HPF.FutureState.Common/DataTransferObjects/DuplicatedCaseLoanDTO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Practices.EnterpriseLibrary.Validation.Validators;
+
+namespace HPF.FutureState.Common.DataTransferObjects
+{
+    [Serializable]
+    public class DuplicatedCaseLoanDTO:BaseDTO
+    {
+        public string LoanNumber { get; set; }
+        public int AgencyID { get; set; }
+        public int FcID { get; set; }
+        public int ServicerID { get; set; }
+        public string ServicerName { get; set; }
+        public string BorrowerFirstName { get; set; }
+        public string BorrowerLastName { get; set; }
+        public string AgencyName { get; set; }
+        public string AgencyCaseNumber { get; set; }
+        public string CounselorFullName { get; set; }
+        public string CounselorPhone { get; set; }
+        public string CounselorEmail { get; set; }
+    }
+}
