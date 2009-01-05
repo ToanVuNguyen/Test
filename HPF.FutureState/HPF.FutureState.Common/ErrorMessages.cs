9dabe98ecac8983ef984822f40f43c7ebf578c8b
t55nguye <t55nguye@6d9d970a-e046-46de-96ce-fbc9f6c46dd9>
2009-01-05 09:07:07 +0000

    git-svn-id: http://nt-svn-jira.vdc.csc.com/HPF/Implementation/Source Code@541 6d9d970a-e046-46de-96ce-fbc9f6c46dd9

--- HPF.FutureState/HPF.FutureState.Common/ErrorMessages.cs
+++ HPF.FutureState/HPF.FutureState.Common/ErrorMessages.cs
@@ -18,6 +18,7 @@ namespace HPF.FutureState.Common
         public const string PROCESSING_EXCEPTION_INVALID_FC_ID_FOR_AGENCY_ID = "Case with pair of Agency Case Number and Agency ID is not in Database";
         public const string PROCESSING_EXCEPTION_MISSING_REQUIRED_FIELD = "Missing required fields";
         public const string MISC_PROCESSING_EXCEPTION = "Miscellaneous processing exception" ;
+        public const string PROCESSING_EXCEPTION_INVALID_CODE = "Invalid code";
 
         private static readonly Dictionary<int, string> errorMessageDict = new Dictionary<int, string>();
 
