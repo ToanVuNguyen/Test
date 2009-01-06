edc8c04120a01c7ea0a5be5d5a2d5acc7d8464e7
t55nguye <t55nguye@6d9d970a-e046-46de-96ce-fbc9f6c46dd9>
2009-01-06 06:08:46 +0000

    git-svn-id: http://nt-svn-jira.vdc.csc.com/HPF/Implementation/Source Code@566 6d9d970a-e046-46de-96ce-fbc9f6c46dd9

--- HPF.FutureState/HPF.FutureState.BusinessLogic/ForeclosureCaseSetBL.cs
+++ HPF.FutureState/HPF.FutureState.BusinessLogic/ForeclosureCaseSetBL.cs
@@ -18,7 +18,7 @@ namespace HPF.FutureState.BusinessLogic
 {
     public class ForeclosureCaseSetBL : BaseBusinessLogic
     {
-        const int NUMBER_OF_ERROR_APP_SEARCH_CRITERIA = 11;
+       
         const string LOAN_1ST = "1ST";
         const string INCOME = "1";
         const string EXPENSES = "2";
@@ -1523,108 +1523,6 @@ namespace HPF.FutureState.BusinessLogic
 
         #endregion
 
-        #region AppForeclosureCaseSearch
-        /// <summary>
-        /// Check if the user has input any info to the search criteria?
-        /// </summary>
-        /// <param name="searchCriteria"></param>
-        /// <returns>true for doing search and false for exception</returns>
-        private bool ValidateSearchCriteria(AppForeclosureCaseSearchCriteriaDTO searchCriteria)
-        {
-            ValidationResults validationResults = HPFValidator.Validate<AppForeclosureCaseSearchCriteriaDTO>(searchCriteria, "AppSearchRequireCriteria");
-            if (validationResults.Count == NUMBER_OF_ERROR_APP_SEARCH_CRITERIA)
-                return false;// user doesnt change anything in search criteria
-            
-            return true;
-        }
-        /// <summary>
-        /// Search Foreclosure Case
-        /// </summary>
-        /// <param name="searchCriteria">Search criteria</param>
-        /// <returns>Collection of AppForeclosureCaseSearchResult</returns>
-        public AppForeclosureCaseSearchResultDTOCollection AppSearchforeClosureCase(AppForeclosureCaseSearchCriteriaDTO searchCriteria)
-        {
-            AppForeclosureCaseSearchResultDTOCollection result = new AppForeclosureCaseSearchResultDTOCollection();
-            Collection<string> ErrorMess = AppRequireFieldValidation(searchCriteria);
-            if (!ValidateSearchCriteria(searchCriteria))
-            {
-                throw new DataValidationException("Please choose argument(s) for at least one search option");
-            }
-            if (ErrorMess != null)
-            {
-               
-                AppThrowMissingRequiredFieldsException(ErrorMess);
-            }
-            result = ForeclosureCaseDAO.CreateInstance().AppSearchForeclosureCase(searchCriteria);
-            return result;
-        }
-        private void AppThrowMissingRequiredFieldsException(Collection<string> collection)
-        {
-            ProcessingException pe = new ProcessingException();
-            foreach (string obj in collection)
-            {
-                ExceptionMessage em = new ExceptionMessage();
-                em.Message = obj;
-                pe.ExceptionMessages.Add(em);
-            }
-            throw pe;
-        }
-
-        #region Functions check validate to app foreclosure case
-        ///<summary>
-        ///1.AgencyCaseID request alpha numberic 
-        ///2.ForclosureCaseID request numberic
-        ///3.LoanNumber request alpha numberic
-        ///4.PropertyZip request 5 digit
-        ///5.Last4SSN request 4 digit
-        /// </summary>
-        Collection<string> AppRequireFieldValidation(AppForeclosureCaseSearchCriteriaDTO searchCriteria)
-        {
-           
-            Collection<string> msgAppForeclosureCaseSearch = new Collection<string>();
-            RequireAppForeclosureCase(searchCriteria, ref msgAppForeclosureCaseSearch, "CriteriaValidation");
-            if (msgAppForeclosureCaseSearch.Count == 0)
-                return null;
-            return msgAppForeclosureCaseSearch;
-        }
-        void RequireAppForeclosureCase(AppForeclosureCaseSearchCriteriaDTO searchCriteria, ref Collection<string> msg, string ruleset)
-        {
-            ValidationResults validationResults = HPFValidator.Validate<AppForeclosureCaseSearchCriteriaDTO>(searchCriteria, ruleset);
-            foreach (ValidationResult result in validationResults)
-            {
-                msg.Add(result.Message);
-            }
-        }
-
-        #endregion
-        /// <summary>
-        /// Get Program Name and Program ID to display in DDLB
-        /// </summary>
-        /// <returns>ProgramDTOCollection containts all Programs</returns>
-        public ProgramDTOCollection GetProgram()
-        {
-            ProgramDTOCollection result = ForeclosureCaseDAO.CreateInstance().AppGetProgram();
-            return result;
-        }
-        /// <summary>
-        /// Get State Name and State ID to display in DDLB
-        /// </summary>
-        /// <returns>StateDTOCollection containts all State</returns>
-        public StateDTOCollection GetState()
-        {
-            StateDTOCollection result = ForeclosureCaseDAO.CreateInstance().AppGetState();
-            return result;
-        }
-        /// <summary>
-        /// Get Agency Name and Agency ID to display in DDLB
-        /// </summary>
-        /// <returns>AgencyDTOCollection containts all Agency</returns>
-        public AgencyDTOCollection GetAgency()
-        {
-            AgencyDTOCollection result = ForeclosureCaseDAO.CreateInstance().AppGetAgency();
-            return result;
-        }
-
-        #endregion
+        
     }
 }
