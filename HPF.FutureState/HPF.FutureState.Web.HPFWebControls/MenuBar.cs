aa585be3a9eca10476d712e46fd577d9a63ac440
stran <stran@6d9d970a-e046-46de-96ce-fbc9f6c46dd9>
2009-01-06 04:13:20 +0000

    Finish read XML file
    git-svn-id: http://nt-svn-jira.vdc.csc.com/HPF/Implementation/Source Code@559 6d9d970a-e046-46de-96ce-fbc9f6c46dd9

--- HPF.FutureState/HPF.FutureState.Web.HPFWebControls/MenuBar.cs
+++ HPF.FutureState/HPF.FutureState.Web.HPFWebControls/MenuBar.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Xml.Linq;
+using System.Xml;
 
 namespace HPF.FutureState.Web.HPFWebControls
 {
@@ -32,18 +33,38 @@ namespace HPF.FutureState.Web.HPFWebControls
         /// <param name="xmlFile"></param>
         public void Load(string xmlFile)
         {
-            //XDocument doc = XDocument.Load("MenuExample.xml");
-            //var menubar = doc.Root;
-            //foreach (var i in menubar)
-            //{ 
-                
-            //}
-            Menu Home = new Menu { Id = "Home", Title = "Home", Url = "", Enabled = true };
-            AddMenu(Home);
-            Menu Accounting = new Menu { Id = "Accounting", Title = "Accounting", Url = "#", Enabled = true };
-            Accounting.AddMenuItem(new MenuItem { Id = "Siny", Title = "Siny", Url = "#", Enabled = true });
-            Accounting.AddMenuItem(new MenuItem { Id = "Siny1", Title = "Siny1", Url = "#", Enabled = false });
-            AddMenu(Accounting);
+
+            XDocument doc = XDocument.Load(@"D:\HPF\24-12-2008\Implementation\Source Code\HPF.FutureState\HPF.FutureState.Web.HPFWebControls\MenuExample.xml");
+            var menus = from menu in doc.Descendants("Menu")
+                        select new Menu
+                        {
+                            Id = menu.Attribute("Id").Value,
+                            Title = menu.Attribute("Title").Value,
+                            Url = menu.Attribute("Url").Value,
+                            Enabled = bool.Parse(menu.Attribute("Enabled").Value)
+                        };
+            foreach (var menu in menus)
+            {
+                var childs = from child in doc.Descendants("MenuItem")
+                             where child.Parent.Attribute("Id").Value == menu.Id
+                             select new MenuItem
+                             {
+                                 Id = child.Attribute("Id").Value,
+                                 Title = child.Attribute("Title").Value,
+                                 Url = child.Attribute("Url").Value,
+                                 Enabled = bool.Parse(child.Attribute("Enabled").Value)
+                             };
+                foreach (MenuItem menuItem in childs)
+                    menu.AddMenuItem(menuItem);
+                AddMenu(menu);
+            }
+            
+            //Menu Home = new Menu { Id = "Home", Title = "Home", Url = "", Enabled = true };
+            //AddMenu(Home);
+            //Menu Accounting = new Menu { Id = "Accounting", Title = "Accounting", Url = "#", Enabled = true };
+            //Accounting.AddMenuItem(new MenuItem { Id = "Siny", Title = "Siny", Url = "#", Enabled = true });
+            //Accounting.AddMenuItem(new MenuItem { Id = "Siny1", Title = "Siny1", Url = "#", Enabled = false });
+            //AddMenu(Accounting);
         }
 
         /// <summary>
