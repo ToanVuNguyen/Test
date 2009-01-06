f1fac24d69b4c499f5d6e313f70bf3c954319711
ltcao <ltcao@6d9d970a-e046-46de-96ce-fbc9f6c46dd9>
2009-01-06 09:23:16 +0000

    Update security framework,
    git-svn-id: http://nt-svn-jira.vdc.csc.com/HPF/Implementation/Source Code@586 6d9d970a-e046-46de-96ce-fbc9f6c46dd9

---
+++ HPF.FutureState/HPF.FutureState.DataAccess/MenuSecurityDAO.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HPF.FutureState.DataAccess
+{
+    public class MenuSecurityDAO : BaseDAO
+    {
+        private static readonly MenuSecurityDAO instance = new MenuSecurityDAO();
+        /// <summary>
+        /// Singleton
+        /// </summary>
+        public static MenuSecurityDAO Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+        protected MenuSecurityDAO()
+        {
+            
+        }
+    }
+}
