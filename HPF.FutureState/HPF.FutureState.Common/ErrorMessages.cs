ad2e841c631772658d0d179a104409be4eed861e
t47nguye <t47nguye@6d9d970a-e046-46de-96ce-fbc9f6c46dd9>
2009-01-07 06:56:52 +0000

    remove ProcessingException
    git-svn-id: http://nt-svn-jira.vdc.csc.com/HPF/Implementation/Source Code@612 6d9d970a-e046-46de-96ce-fbc9f6c46dd9

--- HPF.FutureState/HPF.FutureState.Common/ErrorMessages.cs
+++ HPF.FutureState/HPF.FutureState.Common/ErrorMessages.cs
@@ -11,14 +11,14 @@ namespace HPF.FutureState.Common
 
         public const string PROCESSING_EXCEPTION_NULL_FORECLOSURE_CASE_SET = "Foreclosure case set is null";
         public const string PROCESSING_EXCEPTION_NULL_FORECLOSURE_CASE = "Foreclosure case is null";       
-        public const string PROCESSING_EXCEPTION_DUPLICATE_FORECLOSURE_CASE = "Duplicate Foreclosure case";        
-        public const string PROCESSING_EXCEPTION_INVALID_AGENCY_CASE_NUM_OR_AGENCY_ID= "Either Agency Case Number or Agency ID is invalid";
-        public const string PROCESSING_EXCEPTION_EXISTING_AGENCY_CASE_NUM_AND_AGENCY_ID = "Case with pair of Agency Case Number and Agency ID already existed";
-        public const string PROCESSING_EXCEPTION_INVALID_FC_ID = "Invalid Foreclosure Case ID";
-        public const string PROCESSING_EXCEPTION_INVALID_FC_ID_FOR_AGENCY_ID = "Case with pair of Agency Case Number and Agency ID is not in Database";
-        public const string PROCESSING_EXCEPTION_MISSING_REQUIRED_FIELD = "Missing required fields";
-        public const string MISC_PROCESSING_EXCEPTION = "Miscellaneous processing exception" ;
-        public const string PROCESSING_EXCEPTION_INVALID_CODE = "Invalid code";
+        public const string EXCEPTION_DUPLICATE_FORECLOSURE_CASE = "Duplicate Foreclosure case";        
+        public const string EXCEPTION_INVALID_AGENCY_CASE_NUM_OR_AGENCY_ID= "Either Agency Case Number or Agency ID is invalid";
+        public const string EXCEPTION_EXISTING_AGENCY_CASE_NUM_AND_AGENCY_ID = "Case with pair of Agency Case Number and Agency ID already existed";
+        public const string EXCEPTION_INVALID_FC_ID = "Invalid Foreclosure Case ID";
+        public const string EXCEPTION_INVALID_FC_ID_FOR_AGENCY_ID = "Case with pair of Agency Case Number and Agency ID is not in Database";
+        public const string EXCEPTION_MISSING_REQUIRED_FIELD = "Missing required fields";
+        public const string EXCEPTION_MISCELLANEOUS = "Miscellaneous processing exception" ;
+        public const string EXCEPTION_INVALID_CODE = "Invalid code";
 
         private static readonly Dictionary<int, string> errorMessageDict = new Dictionary<int, string>();
 
