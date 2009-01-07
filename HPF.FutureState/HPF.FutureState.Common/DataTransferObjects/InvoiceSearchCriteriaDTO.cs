1d7776abf4da2ca951bd0bfe77ba252f5fb9cb21
t47nguye <t47nguye@6d9d970a-e046-46de-96ce-fbc9f6c46dd9>
2009-01-07 08:20:10 +0000

    git-svn-id: http://nt-svn-jira.vdc.csc.com/HPF/Implementation/Source Code@625 6d9d970a-e046-46de-96ce-fbc9f6c46dd9

---
+++ HPF.FutureState/HPF.FutureState.Common/DataTransferObjects/InvoiceSearchCriteriaDTO.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HPF.FutureState.Common.DataTransferObjects
+{
+   
+    public class InvoiceSearchCriteriaDTO
+    {
+        public int FundingSourceId { get; set; }
+        public int ServicerId { get; set; }
+        public int ProgramId { get; set; }
+        public DateTime PeriodStart { get; set; }
+        public DateTime PeriodEnd { get; set; }
+        public CustomBoolean Duplicate { get; set; }
+        public CustomBoolean CompleteCase { get; set; }
+        public CustomBoolean AlreadyBill { get; set; }
+        public CustomBoolean ServicerConsent { get; set; }
+        public CustomBoolean FundingConsent { get; set; }
+        public int MaxNumOfCases { get; set; }
+        public string LoanIndicator { get; set; }
+        public string Gender { get; set; }
+        public string Race { get; set; }
+        public CustomBoolean Ethnicity { get; set; }
+        public NumberRange Age { get; set; }
+        public NumberRange HouseholdGrossAnnualIncome { get; set; }
+        public string HouseholdCode { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+    }
+}
