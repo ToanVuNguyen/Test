85fb713ddccb55e272bb37e1a531ed2adaa44f50
t47nguye <t47nguye@6d9d970a-e046-46de-96ce-fbc9f6c46dd9>
2009-01-07 08:34:14 +0000

    git-svn-id: http://nt-svn-jira.vdc.csc.com/HPF/Implementation/Source Code@629 6d9d970a-e046-46de-96ce-fbc9f6c46dd9

---
+++ HPF.FutureState.Webservice.Test/HPF.FutureState.WebService.Test.Web/SessionVariables.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+
+namespace HPF.FutureState.WebService.Test.Web
+{
+    public class SessionVariables
+    {
+        public static string BUDGET_ASSET_COLLECTION = "BudgetAssetCollection";
+        public static string CASE_LOAN_COLLECTION = "CaseLoanCollection";
+        public static string BUDGET_ITEM_COLLECTION = "BudgetItemCollection";
+        public static string ACTIVITY_LOG_COLLECTION = "ActivityLogCollection";
+        public static string OUTCOME_ITEM_COLLECTION = "OutcomeItemCollection";
+        public static string FORECLOSURE_CASE = "ForeclosureCase";
+
+    }
+}
