923c18837fc03d9746e9dbdf2db99912f36689d0
stran <stran@6d9d970a-e046-46de-96ce-fbc9f6c46dd9>
2009-01-08 07:23:46 +0000

    Added GetWebUser
    git-svn-id: http://nt-svn-jira.vdc.csc.com/HPF/Implementation/Source Code@677 6d9d970a-e046-46de-96ce-fbc9f6c46dd9

--- HPF.FutureState/HPF.FutureState.BusinessLogic/SecurityBL.cs
+++ HPF.FutureState/HPF.FutureState.BusinessLogic/SecurityBL.cs
@@ -108,8 +108,8 @@ namespace HPF.FutureState.BusinessLogic
         /// <param name="userName">Username</param>
         /// <returns>UserDTO</returns>
         public UserDTO GetWebUser(string userName)
-        {            
-            return new UserDTO {UserRole = "Admin", FirstName = "First", LastName = "Last"};
+        {
+            return SecurityDAO.Instance.GetWebUser(userName);
         }
 
         #endregion
