c274ff32efac75db860d927de9e042ba6d4fb6d1
t47nguye <t47nguye@6d9d970a-e046-46de-96ce-fbc9f6c46dd9>
2009-01-08 09:23:16 +0000

    git-svn-id: http://nt-svn-jira.vdc.csc.com/HPF/Implementation/Source Code@687 6d9d970a-e046-46de-96ce-fbc9f6c46dd9

---
+++ HPF.FutureState/HPF.FutureState.Common/Utils/DataValidator/RequiredObjectValidator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Practices.EnterpriseLibrary.Common.Configuration;
+using Microsoft.Practices.EnterpriseLibrary.Validation;
+using Microsoft.Practices.EnterpriseLibrary.Validation.Configuration;
+
+namespace HPF.FutureState.Common.Utils.DataValidator
+{
+    [ConfigurationElementType(typeof(CustomValidatorData))]
+    public class RequiredObjectValidator : Validator
+    {
+        public RequiredObjectValidator()
+            : base(null, null)
+        {            
+        }
+
+        protected override string DefaultMessageTemplate
+        {
+            get { return "Required Field"; }
+        }
+
+        protected override void DoValidate(object objectToValidate, object currentTarget, string key, ValidationResults validationResults)
+        {
+            bool isValid = (objectToValidate == null);
+            if (!isValid)
+                LogValidationResult(validationResults, MessageTemplate, currentTarget, key);
+        }
+        //protected override void DoValidate // (string objectToValidate, object currentTarget, string key, ValidationResults validationResults)
+        //{
+        //    bool isValid = false;
+        //    if (objectToValidate == null || objectToValidate == string.Empty)
+        //        isValid = _nullable;
+        //    else
+        //    {
+        //        string pattern = "^\\d{" + _numberOfDigits + "}$";
+        //        Regex exp = new Regex(pattern);
+
+        //        isValid = exp.Match(objectToValidate).Success;
+        //    }
+        //    if (!isValid)
+        //        LogValidationResult(validationResults, MessageTemplate, currentTarget, key);
+        //}
+    }
+}
---
+++ HPF.FutureState/HPF.FutureState.Common/Utils/DataValidator/RequiredObjectValidatorAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Practices.EnterpriseLibrary.Validation;
+using Microsoft.Practices.EnterpriseLibrary.Validation.Validators;
+namespace HPF.FutureState.Common.Utils.DataValidator
+{
+
+    public class RequiredObjectValidatorAttribute : ValidatorAttribute
+    {
+        public RequiredObjectValidatorAttribute()
+        {            
+        }
+        protected override Validator DoCreateValidator(Type targetType)
+        {
+            return new RequiredObjectValidator();
+        }
+    }
+}
