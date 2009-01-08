9d9e1f3717caba244ae9a9dd7a291f06d36b8d24
t47nguye <t47nguye@6d9d970a-e046-46de-96ce-fbc9f6c46dd9>
2009-01-08 02:22:16 +0000

    git-svn-id: http://nt-svn-jira.vdc.csc.com/HPF/Implementation/Source Code@661 6d9d970a-e046-46de-96ce-fbc9f6c46dd9

---
+++ HPF.FutureState/HPF.FutureState.UnitTest/DataAccess/InvoiceDAOTest.cs
@@ -0,0 +1,119 @@
+﻿using HPF.FutureState.DataAccess;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HPF.FutureState.Common.DataTransferObjects;
+using System.Collections.Generic;
+using System.Configuration;
+using System;
+using System.Data.SqlClient;
+
+namespace HPF.FutureState.UnitTest
+{
+    
+    
+    /// <summary>
+    ///This is a test class for InvoiceDAOTest and is intended
+    ///to contain all InvoiceDAOTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class InvoiceDAOTest
+    {
+
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        // 
+        //You can use the following additional attributes as you write your tests:
+        //
+        //Use ClassInitialize to run code before running the first test in the class
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Use ClassCleanup to run code after all tests in a class have run
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize to run code before running each test
+        [TestInitialize()]
+        public void MyTestInitialize()
+        {
+            var dbConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["HPFConnectionString"].ConnectionString);
+            dbConnection.Open();
+            var command = new SqlCommand();
+            command.Connection = dbConnection;
+            command.CommandText = "Insert Into Invoice(funding_source_id, invoice_cd, period_start_dt, period_end_dt, create_dt, create_user_id, create_app_name, chg_lst_dt, chg_lst_user_id, chg_lst_app_name)" +
+                                    " values (1, 'aaaaa', '1/1/2008', '6/1/2008', '1/1/2208', 'test', 'test', '1/1/2008', 'test', 'test')";
+            command.ExecuteNonQuery();
+
+            command.CommandText = "Insert Into Invoice(funding_source_id,invoice_cd, period_start_dt, period_end_dt, create_dt, create_user_id, create_app_name, chg_lst_dt, chg_lst_user_id, chg_lst_app_name)" +
+                                    " values (2, 'bbbbb', '1/1/2008', '6/1/2008', '1/1/2208', 'test', 'test', '1/1/2008', 'test', 'test')";
+            command.ExecuteNonQuery();
+            dbConnection.Close();
+        }
+        //
+        //Use TestCleanup to run code after each test has run
+        [TestCleanup()]
+        public void MyTestCleanup()
+        {
+            var dbConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["HPFConnectionString"].ConnectionString);
+            dbConnection.Open();
+            var command = new SqlCommand();
+            command.Connection = dbConnection;
+            command.CommandText = "Delete Invoice where funding_source_id = 1 and invoice_cd = 'aaaaa'";
+            command.ExecuteNonQuery();
+
+            command.CommandText = command.CommandText = "Delete Invoice where funding_source_id = 2 and invoice_cd = 'bbbbb'";
+            command.ExecuteNonQuery();
+            dbConnection.Close();
+        }
+        //
+        #endregion
+
+
+        /// <summary>
+        ///A test for SearchInvoice
+        ///</summary>
+        [TestMethod()]
+        public void SearchInvoiceTest()
+        {
+            InvoiceDAO target = new InvoiceDAO(); // TODO: Initialize to an appropriate value
+            InvoiceSearchCriteriaDTO searchCriteria = new InvoiceSearchCriteriaDTO(); // TODO: Initialize to an appropriate value
+            searchCriteria.FundingSourceId = 1;
+            searchCriteria.PeriodStart = new DateTime(2008, 01, 01);
+            searchCriteria.PeriodEnd = new DateTime(2008, 06, 01);
+            
+
+            List<int> expected = new List<int>();
+            expected.Add(1);
+            expected.Add(2);
+
+            List<InvoiceSearchResultDTO> results;
+            results = target.SearchInvoice(searchCriteria);
+            List<int> actual = new List<int>();
+            actual.Add(results[0].FundingSourceId);
+            actual.Add(results[1].FundingSourceId);
+            Assert.AreEqual(expected, actual);
+            
+        }
+    }
+}
