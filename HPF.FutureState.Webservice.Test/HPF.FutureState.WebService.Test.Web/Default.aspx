55d7d55b26db4097958245814dd0cf5c3eb698d5
t47nguye <t47nguye@6d9d970a-e046-46de-96ce-fbc9f6c46dd9>
2009-01-08 09:25:09 +0000

    git-svn-id: http://nt-svn-jira.vdc.csc.com/HPF/Implementation/Source Code@690 6d9d970a-e046-46de-96ce-fbc9f6c46dd9

--- HPF.FutureState.Webservice.Test/HPF.FutureState.WebService.Test.Web/Default.aspx
+++ HPF.FutureState.Webservice.Test/HPF.FutureState.WebService.Test.Web/Default.aspx
@@ -2,4 +2,25 @@
 <asp:Content ID="ContentHeader" ContentPlaceHolderID="ContentPlaceHolderHeader" runat="server">
 </asp:Content>
 <asp:Content ID="ContentBody" ContentPlaceHolderID="ContentPlaceHolderBody" runat="server">
+    <asp:Table ID="Table1" runat="server" Visible="false">
+    <asp:TableRow>
+        <asp:TableCell>
+        Username:
+        </asp:TableCell>
+        <asp:TableCell>
+        </asp:TableCell>
+    </asp:TableRow>
+    <asp:TableRow>
+        <asp:TableCell>
+        Password
+        </asp:TableCell>
+        <asp:TableCell>
+        </asp:TableCell>
+    </asp:TableRow>
+    </asp:Table>
+    <asp:LinkButton ID="LinkButton1" runat="server" PostBackUrl="~/SearchForeclosureCase.aspx">Search Foreclosure Case</asp:LinkButton>
+    <br />
+    <asp:LinkButton ID="LinkButton2" runat="server" PostBackUrl="~/InsertCallLog.aspx">Insert Call Log</asp:LinkButton>
+    <br />
+    <asp:LinkButton ID="LinkButton3" runat="server" PostBackUrl="~/RetrieveCallLog.aspx">Retrieve Call Log</asp:LinkButton>
 </asp:Content>
--- HPF.FutureState.Webservice.Test/HPF.FutureState.WebService.Test.Web/Default.aspx.designer.cs
+++ HPF.FutureState.Webservice.Test/HPF.FutureState.WebService.Test.Web/Default.aspx.designer.cs
@@ -12,5 +12,41 @@ namespace HPF.FutureState.WebService.Test.Web {
     
     
     public partial class Default {
+        
+        /// <summary>
+        /// Table1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Table Table1;
+        
+        /// <summary>
+        /// LinkButton1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LinkButton LinkButton1;
+        
+        /// <summary>
+        /// LinkButton2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LinkButton LinkButton2;
+        
+        /// <summary>
+        /// LinkButton3 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LinkButton LinkButton3;
     }
 }
