5471e7a3f7a60185f633fb7cffe1a23afd5ff879
stran <stran@6d9d970a-e046-46de-96ce-fbc9f6c46dd9>
2009-01-09 06:13:13 +0000

    Added webserver control
    git-svn-id: http://nt-svn-jira.vdc.csc.com/HPF/Implementation/Source Code@737 6d9d970a-e046-46de-96ce-fbc9f6c46dd9

---
+++ HPF.FutureState.Webservice.Test/HPF.FutureState.Web.HPFWebControls/BaseMenu.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HPF.FutureState.Web.HPFWebControls
+{
+    [Serializable]
+    public abstract class BaseMenu : IEnumerable<BaseMenu>
+    {
+        public string Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Url { get; set; }
+
+        private bool enabled = true;
+        
+        public bool Enabled
+        {
+            get { return enabled; }
+            set { enabled = value; }
+        }
+        private bool visibled = true;
+
+        public bool Visibled
+        {
+            get { return visibled; }
+            set { visibled = value; }
+        }
+
+        private readonly Dictionary<string, BaseMenu> _ChildMenus;
+
+        protected BaseMenu()
+        {
+            _ChildMenus = new Dictionary<string, BaseMenu>();
+        }
+
+        protected void AddMenu(BaseMenu menu)
+        {
+            _ChildMenus.Add(menu.Id, menu);
+        }
+
+        public void RemoveMenu(string id)
+        {
+            _ChildMenus.Remove(id);
+        }
+        public int Count
+        {
+            get { return _ChildMenus.Count; }
+        }
+        /// <summary>
+        /// Find a child menu by menuId
+        /// </summary>
+        /// <param name="id">Menu Id</param>
+        /// <returns>A Menu</returns>
+        public BaseMenu FindMenu(string id)
+        {
+            return _ChildMenus.ContainsKey(id) ? _ChildMenus[id] : null;
+        }
+
+        #region IEnumerable<BaseMenu> Members
+
+        public IEnumerator<BaseMenu> GetEnumerator()
+        {
+            foreach (var menu in _ChildMenus)
+            {
+                yield return menu.Value;
+            }
+        }
+
+        #endregion
+
+        #region IEnumerable Members
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            foreach (var menu in _ChildMenus)
+            {
+                yield return menu.Value;
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ HPF.FutureState.Webservice.Test/HPF.FutureState.Web.HPFWebControls/MenuBar.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using System.Xml;
+namespace HPF.FutureState.Web.HPFWebControls
+{
+    public class MenuBar: IEnumerable<Menu>
+    {
+        private readonly Dictionary<string, Menu> _MenuList;
+
+        public MenuBar()
+        {
+            _MenuList = new Dictionary<string, Menu>();
+        }
+
+        public void AddMenu(Menu menu)
+        {
+            _MenuList.Add(menu.Id, menu);
+        }
+
+        public void RemoveMenu(string id)
+        {
+            _MenuList.Remove(id);
+        }        
+
+        /// <summary>
+        /// Load menu structure from XmlFile
+        /// </summary>
+        /// <param name="xmlFile"></param>
+        public void Load(string xmlFile)
+        {
+            XDocument doc = XDocument.Load(xmlFile);
+            var menus = from menu in doc.Descendants("Menu")
+                        select new Menu
+                        {
+                            Id = menu.Attribute("Id").Value,
+                            Title = menu.Attribute("Title").Value,
+                            Url = menu.Attribute("Url").Value,
+                            Enabled = bool.Parse(menu.Attribute("Enabled").Value)
+                        };
+            foreach (var menu in menus)
+            {
+                var childs = from child in doc.Descendants("MenuItem")
+                             where child.Parent.Attribute("Id").Value == menu.Id
+                             select new MenuItem
+                             {
+                                 Id = child.Attribute("Id").Value,
+                                 Title = child.Attribute("Title").Value,
+                                 Url = child.Attribute("Url").Value,
+                                 Enabled = bool.Parse(child.Attribute("Enabled").Value)
+                             };
+                foreach (MenuItem menuItem in childs)
+                    menu.AddMenuItem(menuItem);
+                AddMenu(menu);
+            }
+            
+            
+        }
+
+        /// <summary>
+        /// Load menu from database
+        /// </summary>
+        //public void LoadFromDatabase(int userID)
+        //{
+        //    MenuGroupDTOCollection menuBar = MenuGroupBL.Instance.GetMenuGroupCollectionByUserID(userID);
+        //    if (menuBar == null)
+        //    {
+        //        AddMenu(new Menu { Title = string.Empty, Enabled=false, Id="invisible" });
+        //        return;
+        //    }
+            
+        //    Menu menu=null;
+        //    foreach(MenuGroupDTO menuGroup in menuBar)
+        //    {
+        //        menu = new Menu { Id = menuGroup.GroupId.ToString(), Title = menuGroup.GroupName, Url = menuGroup.GroupTarget, Visibled = true };
+        //        if (menuGroup.MenuItemList.Count==0&&menuGroup.GroupTarget=="#")
+        //            menu.Enabled = false;
+        //        foreach (MenuItemDTO menuItem in menuGroup.MenuItemList)
+        //        {
+        //            if (menuItem.Visible)
+        //            {
+        //                MenuItem item = new MenuItem { Id = menuItem.ItemId.ToString(), Title = menuItem.ItemName, Url = menuItem.ItemTarget, Visibled = menuItem.Visible };
+        //                menu.AddMenuItem(item);
+        //            }
+        //        }
+        //        AddMenu(menu);
+        //    }
+        //}
+
+        /// <summary>
+        /// Enabled Menu by menu id
+        /// </summary>
+        /// <param name="id"></param>
+        public void EnabledMenu(string id)
+        {
+            foreach (var m in _MenuList)
+                if (m.Key == id)
+                    m.Value.Enabled = true;
+        }
+
+        /// <summary>
+        /// Disable Menu by menu id
+        /// </summary>
+        /// <param name="id"></param>
+        public void DisabledMenu(string id)
+        {
+            foreach(var m in _MenuList)
+                if(m.Key==id)
+                    m.Value.Enabled=false;
+        }
+
+        #region IEnumerable<Menu> Members
+
+        public IEnumerator<Menu> GetEnumerator()
+        {
+            foreach (var menu in _MenuList)
+            {
+                yield return menu.Value;
+            }
+        }
+
+        #endregion
+
+        #region IEnumerable Members
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            foreach (var menu in _MenuList)
+            {
+                yield return menu.Value;
+            }
+        }
+
+        #endregion
+    }
+}
