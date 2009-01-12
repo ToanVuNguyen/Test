df5f3b8dbb17e0afbb38badd38a4f5e1246c72b1
t47nguye <t47nguye@6d9d970a-e046-46de-96ce-fbc9f6c46dd9>
2009-01-12 10:40:09 +0000

    git-svn-id: http://nt-svn-jira.vdc.csc.com/HPF/Implementation/Source Code@799 6d9d970a-e046-46de-96ce-fbc9f6c46dd9

---
+++ HPF.FutureState/HPF.FutureState.Common/Utils/DataValidator/YesNoIndicatorValidator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Practices.EnterpriseLibrary.Common.Configuration;
+using Microsoft.Practices.EnterpriseLibrary.Validation;
+using Microsoft.Practices.EnterpriseLibrary.Validation.Configuration;
+
+namespace HPF.FutureState.Common.Utils.DataValidator
+{
+    [ConfigurationElementType(typeof(CustomValidatorData))]
+    public class YesNoIndicatorValidator:Validator<string>
+    {
+        const string _YesIndicator = "Y";
+        const string _NoIndicator = "N";
+        public YesNoIndicatorValidator()
+            : base(null, null)
+        {
+            
+        }
+
+        public YesNoIndicatorValidator(string messageTemplate, string tag)
+            : base(messageTemplate, tag)
+        {
+
+        }
+
+        protected override string DefaultMessageTemplate
+        {
+            get { return "InTake day is incorrect."; }
+        }
+
+        protected override void DoValidate(String objectToValidate, object currentTarget, string key, ValidationResults validationResults)
+        {
+            bool isValid = false;
+            isValid = (objectToValidate.ToUpper().Equals(_YesIndicator) || objectToValidate.ToUpper().Equals(_NoIndicator));
+            if (!isValid)
+                LogValidationResult(validationResults, MessageTemplate, currentTarget, key);
+
+            
+        }
+    }
+}
