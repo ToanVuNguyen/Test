fabe8c72c775e631864b3595ad3fc809b1e203ef
t47nguye <t47nguye@6d9d970a-e046-46de-96ce-fbc9f6c46dd9>
2009-01-12 10:00:56 +0000

    git-svn-id: http://nt-svn-jira.vdc.csc.com/HPF/Implementation/Source Code@796 6d9d970a-e046-46de-96ce-fbc9f6c46dd9

---
+++ HPF.FutureState/HPF.FutureState.Common/DataTransferObjects/CallCenterDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HPF.FutureState.Common.DataTransferObjects
+{
+    public class CallCenterDTO : BaseDTO
+    {
+        public int CallCenterID { get; set; }
+        public string CallCenterName { get; set; }
+    }
+}
