bd6c9ccc56e79030d2407d73a1332946549efd0b
stran <stran@6d9d970a-e046-46de-96ce-fbc9f6c46dd9>
2009-01-12 08:09:31 +0000

    Added 2 more DTOs
    git-svn-id: http://nt-svn-jira.vdc.csc.com/HPF/Implementation/Source Code@784 6d9d970a-e046-46de-96ce-fbc9f6c46dd9

---
+++ HPF.FutureState/HPF.FutureState.Common/DataTransferObjects/FundingSourceDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HPF.FutureState.Common.DataTransferObjects
+{
+    public class FundingSourceDTO:BaseDTO
+    {
+        public int FundingSourceID { get; set; }
+        public string FundingSourceName { get; set; }
+    }
+}
---
+++ HPF.FutureState/HPF.FutureState.Common/DataTransferObjects/FundingSourceDTOCollection.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HPF.FutureState.Common.DataTransferObjects
+{
+    public class FundingSourceDTOCollection:BaseDTOCollection<FundingSourceDTO>
+    {
+    }
+}
