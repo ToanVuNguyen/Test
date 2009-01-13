3b842de220340018b59a909533ece6404199ba53
t55nguye <t55nguye@6d9d970a-e046-46de-96ce-fbc9f6c46dd9>
2009-01-13 02:40:53 +0000

    update fields
    git-svn-id: http://nt-svn-jira.vdc.csc.com/HPF/Implementation/Source Code@820 6d9d970a-e046-46de-96ce-fbc9f6c46dd9

--- HPF.FutureState/HPF.FutureState.Common/DataTransferObjects/AgencyPayableDTO.cs
+++ HPF.FutureState/HPF.FutureState.Common/DataTransferObjects/AgencyPayableDTO.cs
@@ -8,18 +8,20 @@ namespace HPF.FutureState.Common.DataTransferObjects
     [Serializable]
     public class AgencyPayableDTO: BaseDTO
     {
-        public int AgencyPayableId { get; set; }
+        
         public int AgencyId { get; set; }
-        public string AgencyName { get; set; }
-        public DateTime PaymentDate { get; set; }
-        public string PayamentCode { get; set; }
-        public string StatusCode { get; set; }
         public DateTime PeriodStartDate { get; set; }
         public DateTime PeriodEndDate { get; set; }
-        public string PaymentComment { get; set; }
+        
+        public string PayamentCode { get; set; }
         public string AccountLinkTBD { get; set; }
         public decimal AgencyPayablePaymentAmount { get; set; }
-
+        public string PaymentComment { get; set; }
+        public string StatusCode { get; set; }
+        public DateTime PaymentDate { get; set; }
+        //---------------
+        public int AgencyPayableId { get; set; }
+        //public DateTime PaymentDate { get; set; }
         public AgencyPayableCaseDTOCollection AgencyPayableCases { get; set; }
     }
 }
