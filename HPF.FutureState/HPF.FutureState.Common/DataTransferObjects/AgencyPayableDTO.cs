20f20e486dffedc58442a6bf2354d39a7a98e04c
t55nguye <t55nguye@6d9d970a-e046-46de-96ce-fbc9f6c46dd9>
2009-01-14 08:01:32 +0000

    change some properties
    git-svn-id: http://nt-svn-jira.vdc.csc.com/HPF/Implementation/Source Code@882 6d9d970a-e046-46de-96ce-fbc9f6c46dd9

--- HPF.FutureState/HPF.FutureState.Common/DataTransferObjects/AgencyPayableDTO.cs
+++ HPF.FutureState/HPF.FutureState.Common/DataTransferObjects/AgencyPayableDTO.cs
@@ -10,12 +10,15 @@ namespace HPF.FutureState.Common.DataTransferObjects
     {
         
         public int AgencyId { get; set; }
+        public string AgencyName { get; set; }
         public DateTime PeriodStartDate { get; set; }
         public DateTime PeriodEndDate { get; set; }
         
         public string PayamentCode { get; set; }
         public string AccountLinkTBD { get; set; }
         public decimal AgencyPayablePaymentAmount { get; set; }
+        //public int TotalCases { get; set; }
+        public decimal TotalAmount { get; set; }
         public string PaymentComment { get; set; }
         public string StatusCode { get; set; }
         public DateTime PaymentDate { get; set; }
