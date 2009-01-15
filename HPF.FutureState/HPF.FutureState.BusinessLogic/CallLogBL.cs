7df006aa8ce278b603d0899ad6a5d24e6405468a
kdo <kdo@6d9d970a-e046-46de-96ce-fbc9f6c46dd9>
2009-01-15 07:06:58 +0000

    remove redundant using
    git-svn-id: http://nt-svn-jira.vdc.csc.com/HPF/Implementation/Source Code@945 6d9d970a-e046-46de-96ce-fbc9f6c46dd9

--- HPF.FutureState/HPF.FutureState.BusinessLogic/CallLogBL.cs
+++ HPF.FutureState/HPF.FutureState.BusinessLogic/CallLogBL.cs
@@ -4,10 +4,7 @@ using HPF.FutureState.Common.DataTransferObjects;
 using HPF.FutureState.Common.Utils.DataValidator;
 using HPF.FutureState.Common.Utils.Exceptions;
 using HPF.FutureState.DataAccess;
-
-
 using Microsoft.Practices.EnterpriseLibrary.Validation;
-using HPF.FutureState.Common.Utils.DataValidator;
 using HPF.FutureState.Common;
 
 using System.Collections.Generic;
