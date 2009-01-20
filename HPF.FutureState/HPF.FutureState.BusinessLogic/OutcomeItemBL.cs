98741c53b1382b018b82093f421228cb9b0899df
t47nguye <t47nguye@6d9d970a-e046-46de-96ce-fbc9f6c46dd9>
2009-01-20 11:21:27 +0000

    git-svn-id: http://nt-svn-jira.vdc.csc.com/HPF/Implementation/Source Code@1135 6d9d970a-e046-46de-96ce-fbc9f6c46dd9

---
+++ HPF.FutureState/HPF.FutureState.BusinessLogic/OutcomeItemBL.cs
@@ -0,0 +1,57 @@
+﻿using HPF.FutureState.Common.DataTransferObjects.WebServices;
+using HPF.FutureState.Common.BusinessLogicInterface;
+using HPF.FutureState.Common.DataTransferObjects;
+using HPF.FutureState.Common.Utils.DataValidator;
+using HPF.FutureState.Common.Utils.Exceptions;
+using HPF.FutureState.DataAccess;
+using Microsoft.Practices.EnterpriseLibrary.Validation;
+using HPF.FutureState.Common;
+
+using System.Collections.Generic;
+using System;
+
+namespace HPF.FutureState.BusinessLogic
+{
+    public class OutcomeItemBL : BaseBusinessLogic
+    {
+        private static readonly OutcomeItemBL instance = new OutcomeItemBL();
+        /// <summary>
+        /// Singleton
+        /// </summary>
+        public static OutcomeItemBL Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+        protected OutcomeItemBL()
+        {
+
+        }
+
+        public OutcomeItemDTOCollection RetrieveOutcomeItems(int fcId)
+        {
+            return OutcomeItemDAO.Instance.RetrieveOutcomeItems(fcId);
+        }
+
+        public bool DeleteOutcomeItem(int outcomeItemId, string workingUserId)
+        {
+            OutcomeItemDTO item = new OutcomeItemDTO();
+            item.OutcomeItemId = outcomeItemId;
+            item.SetUpdateTrackingInformation(workingUserId);
+            return OutcomeItemDAO.Instance.DeleteOutcomeItem(item);
+        }
+
+        public bool InstateOutcomeItem(int outcomeItemId, string workingUserId)
+        {
+            OutcomeItemDTO item = new OutcomeItemDTO();
+            item.OutcomeItemId = outcomeItemId;
+            item.SetUpdateTrackingInformation(workingUserId);
+            return OutcomeItemDAO.Instance.InstateOutcomeItem(item);
+        }
+        
+
+    }
+}
