35a99e74af27b2ce1033b261ad681baf8f3c6836
stran <stran@6d9d970a-e046-46de-96ce-fbc9f6c46dd9>
2009-01-21 01:56:42 +0000

    Added Budget Detail DTO
    git-svn-id: http://nt-svn-jira.vdc.csc.com/HPF/Implementation/Source Code@1136 6d9d970a-e046-46de-96ce-fbc9f6c46dd9

---
+++ HPF.FutureState/HPF.FutureState.Common/DataTransferObjects/BudgetDetailDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HPF.FutureState.Common.DataTransferObjects
+{
+    public class BudgetDetailDTO:BaseDTO
+    {
+        public string BudgetCategory { get; set; }
+        public string BudgetSubCategory { get; set; }
+        public decimal Amount { get; set; }
+        public string Note { get; set; }
+    }
+}
