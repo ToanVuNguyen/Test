3c4b9e9a104302468df795a82f4eda282521dc19
t47nguye <t47nguye@6d9d970a-e046-46de-96ce-fbc9f6c46dd9>
2009-02-02 02:56:45 +0000

    remove [XMLIgnore] CallLogDTO
    git-svn-id: http://nt-svn-jira.vdc.csc.com/HPF/Implementation/Source Code@1273 6d9d970a-e046-46de-96ce-fbc9f6c46dd9

--- HPF.FutureState/HPF.FutureState.Common/DataTransferObjects/CallLogDTO.cs
+++ HPF.FutureState/HPF.FutureState.Common/DataTransferObjects/CallLogDTO.cs
@@ -12,112 +12,84 @@ namespace HPF.FutureState.Common.DataTransferObjects
     {
 
         #region property
-        [XmlIgnore]
         public int CallId { get; set; }
-        [XmlIgnore]
         public int CallCenterID { get; set; }
 
-        [XmlIgnore]
         [NullableOrStringLengthValidator(true, 55, "Cc Agent Id Key", Ruleset = "Default")]
         public string CcAgentIdKey { get; set; }
 
         
-        [XmlIgnore]
         [RequiredObjectValidator(Ruleset = "Default", MessageTemplate = ErrorMessages.ERR0351)]
         public DateTime StartDate { get; set; }
 
-        [XmlIgnore]
         [RequiredObjectValidator(Ruleset = "Default", MessageTemplate = ErrorMessages.ERR0352)]
         public DateTime EndDate { get; set; }
         
-        [XmlIgnore]
         [NullableOrStringLengthValidator(true, 10, "DNIS", Ruleset = "Default")]
         public string DNIS { get; set; }
 
-        [XmlIgnore]
         public string CallCenter { get; set; }
 
-        [XmlIgnore]
         [NullableOrStringLengthValidator(true, 15, "Call Source Code", Ruleset = "Default")]        
         public string CallSourceCd { get; set; }
 
-        [XmlIgnore]
         [NullableOrStringLengthValidator(true, 75, "Reason To Call", Ruleset = "Default")]
         public string ReasonToCall { get; set; }
 
-        [XmlIgnore]
         [NullableOrStringLengthValidator(true, 30, "Loan Account Number", Ruleset = "Default")]
         public string LoanAccountNumber { get; set; }
 
-        [XmlIgnore]
         [NullableOrStringLengthValidator(true, 30, "First name", Ruleset = "Default")]
         public string FirstName { get; set; }
 
-        [XmlIgnore]
         [NullableOrStringLengthValidator(true, 30, "Last name", Ruleset = "Default")]
         public string LastName { get; set; }
 
-        [XmlIgnore]
         [NullableOrInRangeValidator(true, "[0-9]", Ruleset = "Default", MessageTemplate = "Servicer Id must be type of Integer")]
         public int ServicerId { get; set; }
 
-        [XmlIgnore]        
         public string OtherServicerName { get; set; }
 
-        [XmlIgnore]        
         [NullableOrStringLengthValidator(true, 9, "Prop Zip", Ruleset = "Default")]
         public string PropZipFull9 { get; set; }
 
-        [XmlIgnore]        
         [NullableOrInRangeValidator(true, "[0-9]", Ruleset = "Default", MessageTemplate = "Prev Agency Id must be type of Integer")]
         public int PrevAgencyId { get; set; }
 
-        [XmlIgnore]        
         [NullableOrStringLengthValidator(true, 20, "Selected Agency Id", Ruleset = "Default")]
         public string SelectedAgencyId { get; set; }
 
-        [XmlIgnore]        
         [NullableOrStringLengthValidator(true, 2000, "Screen Rout", Ruleset = "Default")]
         public string ScreenRout { get; set; }
 
-        [XmlIgnore]
         [NullableOrStringLengthValidator(false, 15, "Final Dispo Code", Ruleset = "Default", MessageTemplate = ErrorMessages.ERR0353)]
         public string FinalDispoCd { get; set; }
 
-        [XmlIgnore]        
         [NullableOrStringLengthValidator(true, 10, "Trans Number", Ruleset = "Default")]
         public string TransNumber { get; set; }
 
-        [XmlIgnore]        
         [NullableOrStringLengthValidator(false, 18, "Cc Call Key", Ruleset = "Default", MessageTemplate = ErrorMessages.ERR0350)]
         public string CcCallKey { get; set; }
 
-        [XmlIgnore]        
         [NullableOrStringLengthValidator(true, 15, "Loan Delinq Status Cd", Ruleset = "Default")]
         public string LoanDelinqStatusCd { get; set; }
 
-        [XmlIgnore]        
         [NullableOrStringLengthValidator(true, 40, "Selected Counselor", Ruleset = "Default")]
         public string SelectedCounselor { get; set; }
 
         
         string _homeownerInd;
-
-        [XmlIgnore]        
         [YesNoIndicatorValidator(true, Ruleset = "Default")]
         public string HomeownerInd { get { return _homeownerInd; } set { _homeownerInd = value.ToUpper(); } }
 
         string _powerOfAttorneyInd;
-        [XmlIgnore]        
         [YesNoIndicatorValidator(true, Ruleset = "Default")]
         public string PowerOfAttorneyInd { get { return _powerOfAttorneyInd; } set { _powerOfAttorneyInd = value.ToUpper(); } }
         
         string _authorizedInd;
-        [XmlIgnore]        
         [YesNoIndicatorValidator(true, Ruleset = "Default")]
         public string AuthorizedInd { get { return _authorizedInd; } set { _authorizedInd = value.ToUpper(); } }
 
-        [XmlIgnore]        
         [NullableOrStringLengthValidator(false, 30, "Working User ID", Ruleset= "Default")]
         public string WorkingUserId { get; set; }
         #endregion        
