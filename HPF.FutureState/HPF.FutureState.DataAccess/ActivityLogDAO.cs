a7e32b99c3f031dddb1d1fb1148f625731200a2c
t55nguye <t55nguye@6d9d970a-e046-46de-96ce-fbc9f6c46dd9>
2009-02-03 10:46:55 +0000

    summary email
    git-svn-id: http://nt-svn-jira.vdc.csc.com/HPF/Implementation/Source Code@1337 6d9d970a-e046-46de-96ce-fbc9f6c46dd9

---
+++ HPF.FutureState/HPF.FutureState.DataAccess/ActivityLogDAO.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+
+using HPF.FutureState.Common.DataTransferObjects;
+using HPF.FutureState.Common.Utils.Exceptions;
+
+namespace HPF.FutureState.DataAccess
+{
+    public class ActivityLogDAO : BaseDAO
+    {
+        private static readonly ActivityLogDAO instance = new ActivityLogDAO();
+        /// <summary>
+        /// Singleton
+        /// </summary>
+        public static ActivityLogDAO Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+        protected ActivityLogDAO()
+        {
+        }
+        public void InsertActivityLog(ActivityLogDTO activityLog)
+        {
+            var dbConnection=CreateConnection();
+            var command = CreateSPCommand("hpf_activity_log_insert",dbConnection);
+            //<Parameter>
+            var sqlParam = new SqlParameter[10];
+            sqlParam[0] = new SqlParameter("@pi_fc_id", activityLog.FcId);
+            sqlParam[1] = new SqlParameter("@pi_activity_cd", activityLog.ActivityCd);
+            sqlParam[2] = new SqlParameter("@pi_activity_dt", activityLog.ActivityDt);
+            sqlParam[3] = new SqlParameter("@pi_activity_note", activityLog.ActivityNote);
+            sqlParam[4] = new SqlParameter("@pi_create_dt", NullableDateTime(activityLog.CreateDate));
+            sqlParam[5] = new SqlParameter("@pi_create_user_id", activityLog.CreateUserId);
+            sqlParam[6] = new SqlParameter("@pi_create_app_name", activityLog.CreateAppName);
+            sqlParam[7] = new SqlParameter("@pi_chg_lst_dt", NullableDateTime(activityLog.ChangeLastDate));
+            sqlParam[8] = new SqlParameter("@pi_chg_lst_user_id", activityLog.ChangeLastUserId);
+            sqlParam[9] = new SqlParameter("@pi_chg_lst_app_name", activityLog.ChangeLastAppName);
+            
+            command.Parameters.AddRange(sqlParam);
+            //</Parameter>
+            try
+            {
+                dbConnection.Open();
+                command.ExecuteNonQuery();
+            }
+            catch (Exception Ex)
+            {
+                throw ExceptionProcessor.Wrap<DataAccessException>(Ex);
+            }
+            finally
+            {
+            dbConnection.Close();
+            }
+        }
+        }
+    }
+
--- HPF.FutureState/HPF.FutureState.Web/SummaryEmail/SummaryEmailUC.ascx.cs
+++ HPF.FutureState/HPF.FutureState.Web/SummaryEmail/SummaryEmailUC.ascx.cs
@@ -14,6 +14,10 @@ using System.Net.Mail;
 using HPF.FutureState.Common.DataTransferObjects;
 using HPF.FutureState.BusinessLogic;
 using System.Globalization;
+using HPF.FutureState.Web.Security;
+using HPF.FutureState.Common.Utils.Exceptions;
+
+
 namespace HPF.FutureState.Web.SummaryEmail
 {
     public partial class SummaryEmailUC : System.Web.UI.UserControl
@@ -21,7 +25,7 @@ namespace HPF.FutureState.Web.SummaryEmail
         protected void Page_Load(object sender, EventArgs e)
         {
             txtTo.Focus();
-            ForeclosureCaseDTO forclosureInfo =(ForeclosureCaseDTO) Session["foreclosureInfo"];
+            ForeclosureCaseDTO forclosureInfo = (ForeclosureCaseDTO)Session["foreclosureInfo"];
             txtSubject.Text = GenerateSubject(forclosureInfo);
         }
         public string SendEmailWithAttachment(string SendFrom, string SendTo, string Subject, string Body)
@@ -56,16 +60,17 @@ namespace HPF.FutureState.Web.SummaryEmail
                         em.IsBodyHtml = true;
 
                         SmtpClient smtp = new SmtpClient();
-                        smtp.Host =ConfigurationManager.AppSettings["smtpServer"];
+                        smtp.Host = ConfigurationManager.AppSettings["smtpServer"];
                         smtp.UseDefaultCredentials = true;
                         smtp.Port = 25;
                         smtp.Send(em);
-                        
+
                         return "Email was sent: " + to + ".";
                     }
                     catch (Exception ex)
                     {
                         return ex.Message;
+                        ExceptionProcessor.HandleException(ex, HPFWebSecurity.CurrentIdentity.LoginName);
                     }
                 }
                 else
@@ -76,6 +81,7 @@ namespace HPF.FutureState.Web.SummaryEmail
             catch (Exception ex)
             {
                 return ex.Message;
+                ExceptionProcessor.HandleException(ex, HPFWebSecurity.CurrentIdentity.LoginName);
             }
         }
 
@@ -84,9 +90,23 @@ namespace HPF.FutureState.Web.SummaryEmail
             //string SendFrom = "HPF.DoNotReply@HopeNetAdmin.org";
             string SendFrom = "tnguyen243@csc.com";
             string SendTo = txtTo.Text;
-            string Subject = txtSubject.Text+"$S$";
+            string Subject = txtSubject.Text + "$S$";
             string Body = txtBody.Text;
             lblMessgage.Text = SendEmailWithAttachment(SendFrom, SendTo, Subject, Body);
+            ActivityLogDTO activityLog = GetActivityLogInfo();
+            activityLog.SetInsertTrackingInformation(HPFWebSecurity.CurrentIdentity.UserId.ToString());
+            ActivityLogBL.Instance.InsertActivityLog(activityLog);
+            
+        }
+        protected ActivityLogDTO GetActivityLogInfo()
+        {
+            ActivityLogDTO activityLog = new ActivityLogDTO();
+            ForeclosureCaseDTO forclosureInfo = (ForeclosureCaseDTO)Session["foreclosureInfo"];
+            activityLog.FcId = forclosureInfo.FcId;
+            activityLog.ActivityCd = "EMAIL";
+            activityLog.ActivityDt = DateTime.Now;
+            activityLog.ActivityNote = string.Concat(" Subject: ", txtSubject.Text, " To: ", txtTo.Text, " Body: ", txtBody.Text);
+            return activityLog;
         }
         /// <summary>
         /// 
@@ -97,12 +117,12 @@ namespace HPF.FutureState.Web.SummaryEmail
         {
             string strSubject = "HPF Summary loan#";
             //get info from case loan to get: Loan Status and Loan Num
-            CaseLoanDTOCollection caseLoanDTOCol=CaseLoanBL.Instance.RetrieveCaseLoan(foreclosurecaseInfo.FcId);
+            CaseLoanDTOCollection caseLoanDTOCol = CaseLoanBL.Instance.RetrieveCaseLoan(foreclosurecaseInfo.FcId);
             //
             strSubject += caseLoanDTOCol[0].AcctNum + "/" + foreclosurecaseInfo.PropZip;
-            
-            string loanDelinqStatus=caseLoanDTOCol[0].LoanDelinqStatusCd;
-            if (foreclosurecaseInfo.ForSaleInd == "Y" ||loanDelinqStatus=="120+" )
+
+            string loanDelinqStatus = caseLoanDTOCol[0].LoanDelinqStatusCd;
+            if (foreclosurecaseInfo.ForSaleInd == "Y" || loanDelinqStatus == "120+")
                 strSubject += " ,priority URGENT";
             return strSubject;
 
