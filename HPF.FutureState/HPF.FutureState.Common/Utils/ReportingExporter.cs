06182564a63219382266aa7197d18bdd6512b6ce
ltcao <ltcao@6d9d970a-e046-46de-96ce-fbc9f6c46dd9>
2009-02-04 07:26:21 +0000

    Create Exporting service export component.
    git-svn-id: http://nt-svn-jira.vdc.csc.com/HPF/Implementation/Source Code@1361 6d9d970a-e046-46de-96ce-fbc9f6c46dd9

---
+++ HPF.FutureState/HPF.FutureState.Common/Utils/ReportingExporter.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Text;
+using System.Net;
+
+namespace HPF.FutureState.Common.Utils
+{
+    /// <summary>
+    /// Export a report from Reporting service server.
+    /// </summary>
+    public class ReportingExporter
+    {        
+        /// <summary>
+        /// Report path
+        /// </summary>
+        public string ReportPath { get; set; }
+
+        /// <summary>
+        /// Reporting server address
+        /// </summary>
+        private readonly string _ReportServer;
+
+        private readonly Dictionary<string, string> _Parameter;
+
+        public ReportingExporter()
+        {
+            _Parameter = new Dictionary<string, string>();
+            _ReportServer = ConfigurationManager.AppSettings["REPORTSERVER_URL"];
+        }
+        /// <summary>
+        /// Export report to Pdf format.
+        /// </summary>
+        /// <returns></returns>
+        public byte[] ExportToPdf()
+        {
+            Validate();
+            var format = "PDF";
+            return Export(format);
+        }        
+
+        /// <summary>
+        /// Export report to Excel format
+        /// </summary>
+        /// <returns></returns>
+        public byte[] ExportToExcel()
+        {
+            Validate();
+            var format = "EXCEL";
+            return Export(format);
+        }
+
+        private void Validate()
+        {
+            if (string.IsNullOrEmpty(ReportPath))
+            {
+                throw new Exception("ReportPath can not be empty.");
+            }
+        }
+
+        private byte[] Export(string format)
+        {
+            var webClient = GetWebClient();            
+            var buffer = webClient.DownloadData(GetReportUrl(format));
+            webClient.Dispose();
+            return buffer;
+        }
+
+        private static WebClient GetWebClient()
+        {
+            var configLoginName = ConfigurationManager.AppSettings["REPORTSERVER_LOGINNAME"];
+            var loginNameDomain = configLoginName.Split('\\');
+            var credentials = new NetworkCredential();
+            if (loginNameDomain.Length > 1)
+            {
+                credentials.UserName = loginNameDomain[1];
+                credentials.Domain = loginNameDomain[0];
+            }
+            else
+                credentials.UserName = configLoginName;
+            //
+            credentials.Password = ConfigurationManager.AppSettings["REPORTSERVER_PASSWORD"];
+
+            return new WebClient
+                       {
+                           Credentials = credentials
+                       };
+        }
+
+        public void SetReportParameter(string name, string value)
+        {
+            if (!_Parameter.ContainsKey(name))
+                _Parameter.Add(name, value);
+            else
+            {
+                _Parameter[name] = value;
+            }
+        }
+
+        private string GetReportUrl(string renderFormat)
+        {
+            var url = _ReportServer + "?/" + ReportPath + "&rs:Command=Render&rs:Format={0}{1}";
+            var paramString = GetParamString();
+            return string.Format(url, renderFormat, paramString);
+        }
+
+        private string GetParamString()
+        {
+            var paramString = new StringBuilder();
+            foreach (var param in _Parameter)
+            {
+                paramString.Append("&" + param.Key + "=" + param.Value);
+            }
+            return paramString.ToString();
+        }
+    }
+}
