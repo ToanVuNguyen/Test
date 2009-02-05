50f31c58ae5c34c7c81ff30113dd7ff33c823d1d
t55nguye <t55nguye@6d9d970a-e046-46de-96ce-fbc9f6c46dd9>
2009-02-05 02:52:45 +0000

    add invoice payment
    git-svn-id: http://nt-svn-jira.vdc.csc.com/HPF/Implementation/Source Code@1409 6d9d970a-e046-46de-96ce-fbc9f6c46dd9

---
+++ HPF.FutureState/HPF.FutureState.Web/InvoicePayments/InvoicePaymentsUC.ascx
@@ -0,0 +1,114 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="InvoicePaymentsUC.ascx.cs" Inherits="HPF.FutureState.Web.InvoicePayments.InvoicePaymentsUC" %>
+
+<link href="../Styles/HPF.css" rel="stylesheet" type="text/css" />
+<asp:ScriptManager ID="myScript" runat="server"></asp:ScriptManager>
+<asp:UpdatePanel ID="myUPanel" runat="server">
+<ContentTemplate>
+
+<table width="90%">
+    <colgroup>
+    <col width="10%" />
+    <col width="10%" />
+    <col width="10%" />
+    <col width="20%" />
+    <col width="50%" />
+    </colgroup>
+    <tr>
+        <td colspan="5" class="Header">
+            Invoice Payments</td>
+    </tr>
+    <tr>
+        <td class="sidelinks" align="right">
+            Funding Source:</td>
+        <td>
+            <asp:DropDownList ID="ddlFundingSource" runat="server" Height="16px" CssClass="Text">
+            </asp:DropDownList>
+        </td>
+        <td class="sidelinks" align="right">
+            Period Start:</td>
+        <td>
+            <asp:TextBox ID="txtPeriodStart" runat="server" CssClass="Text" Width="150px" MaxLength="100"></asp:TextBox>
+        </td>
+        <td>
+            &nbsp;</td>
+    </tr>
+    <tr>
+        <td >
+            </td>
+        <td>
+            </td>
+        <td  class="sidelinks" align="right">
+            Period End:</td>
+        <td >
+            <asp:TextBox ID="txtPeriodEnd" runat="server" Width="150px" MaxLength="100" CssClass="Text"></asp:TextBox>
+        </td>
+        <td >
+            <asp:Button ID="btnRefreshList" runat="server" Text="Refresh List"  
+                Width="100px" CssClass="MyButton" onclick="btnRefreshList_Click"/>
+        </td>
+    </tr>
+    <tr>
+    <td colspan="5">
+    <asp:Label ID="lblMessage" runat="server" Text=""  CssClass="ErrorMessage" ></asp:Label>
+    <asp:RequiredFieldValidator ID="reqtxtPeriodStart"  Display="Dynamic" runat="server" ErrorMessage="Must input date" ControlToValidate="txtPeriodStart"></asp:RequiredFieldValidator>
+    <asp:RequiredFieldValidator ID="reqtxtPeriodEnd" Display="Dynamic"  runat="server" ErrorMessage="Must input date" ControlToValidate="txtPeriodEnd"></asp:RequiredFieldValidator>
+    <asp:CompareValidator ID="cmptxtPeriodStart" runat="server" Display="Dynamic" ErrorMessage="Input correct date format" ControlToValidate="txtPeriodStart" ValueToCompare="1/1/1900" Operator="GreaterThan" Type="Date"></asp:CompareValidator>
+    <asp:CompareValidator ID="cmptxtPeriodEnd" Display="Dynamic" runat="server" ErrorMessage="Input correct date format" ControlToValidate="txtPeriodEnd" ValueToCompare="1/1/1900" Operator="GreaterThan" Type="Date"></asp:CompareValidator>
+    </td></tr><tr>
+        <td colspan="5" class="sidelinks" >
+            Invoice List:</td></tr><tr>
+        <td colspan="4">
+       
+       
+        <asp:Panel ID="panInvoiceList" runat="server"  CssClass="ScrollTable" Width="840px">
+        <asp:GridView ID="grvInvoicePaymentList" runat="server"  BorderStyle="None" Width="100%"   
+                AutoGenerateColumns="false" 
+                onselectedindexchanged="grvInvoiceList_SelectedIndexChanged"   >
+        <HeaderStyle CssClass="FixedHeader" />
+        <AlternatingRowStyle CssClass="AlternatingRowStyle" />
+        <RowStyle CssClass="RowStyle" />
+        <SelectedRowStyle CssClass="SelectedRowStyle" />
+        <Columns>
+        <asp:BoundField HeaderText="Funding Source" DataField="FundingSourceName" />
+        <asp:BoundField HeaderText="Payment ID" DataField="InvoicePaymentID" />
+        <asp:BoundField HeaderText="Payment#" DataField="PaymentNum" />
+        <asp:BoundField HeaderText="Payment Date" DataField="PaymentDate" DataFormatString="{0:d}" />
+        <asp:BoundField HeaderText="Payable Code" DataField="PaymentCode"/>
+        <asp:BoundField HeaderText="Payment Amt" DataField="PaymentAmount" DataFormatString="{0:C}"  ItemStyle-HorizontalAlign="Right"/>
+        <%--<asp:BoundField HeaderText="Comments" DataField="PaymentComment" />--%>
+        </Columns>
+        <EmptyDataTemplate> There is no data match.</EmptyDataTemplate>
+        </asp:GridView>
+        </asp:Panel>
+        
+        </td>
+        <td  valign="top">
+            <table style="vertical-align:top;">
+                <tr>
+                    <td>
+                        <asp:Button ID="btnNewPayable" runat="server" Text="New Payable" Width="150px" 
+                            CssClass="MyButton" />
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <asp:Button ID="btnViewEditPayable" runat="server" Text="View/Edit Payable" 
+                            CssClass="MyButton" Width="150px" onclick="btnViewPayable_Click" />
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        &nbsp;</td>
+                </tr>
+            </table>
+        </td>
+    </tr>
+    <tr>
+        <td>
+            &nbsp;</td><td>
+            &nbsp;</td><td>
+            &nbsp;</td><td>
+            &nbsp;</td><td>
+            &nbsp;</td></tr></table>
+            </ContentTemplate>
+</asp:UpdatePanel>
---
+++ HPF.FutureState/HPF.FutureState.Web/InvoicePayments/InvoicePaymentsUC.ascx.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+using HPF.FutureState.BusinessLogic;
+using HPF.FutureState.Common;
+using HPF.FutureState.Common.DataTransferObjects;
+using HPF.FutureState.Common.Utils.Exceptions;
+using HPF.FutureState.Web.Security;
+
+namespace HPF.FutureState.Web.InvoicePayments
+{
+    public partial class InvoicePaymentsUC : System.Web.UI.UserControl
+    {
+        
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //ApplySecurity();
+            for (int i = 0; i < grvInvoicePaymentList.Rows.Count; i++)
+            {
+                grvInvoicePaymentList.Rows[i].Attributes.Add("onclick", Page.ClientScript.GetPostBackEventReference(grvInvoicePaymentList, "Select$" + i));
+            }
+            if (grvInvoicePaymentList.SelectedIndex != -1)
+                btnViewEditPayable.Attributes.Clear();
+            if (!IsPostBack)
+            {
+                BindFundingSourceDropDownList();
+                SetDefaultPeriodStartEnd();
+                BindGrvInvoicePaymentList(DateTime.Now.AddMonths(-6), DateTime.Now);
+            }
+        }
+        private void ApplySecurity()
+        {
+            if (!HPFWebSecurity.CurrentIdentity.CanView(Constant.MENU_ITEM_TARGET_AGENCY_ACCOUNT_PAYABLE))
+            {
+                Response.Redirect("ErrorPage.aspx?CODE=ERR999");
+            }
+            if (!HPFWebSecurity.CurrentIdentity.CanEdit(Constant.MENU_ITEM_TARGET_AGENCY_ACCOUNT_PAYABLE))
+            {
+                btnNewPayable.Enabled = false;
+                btnViewEditPayable.Enabled = false;
+            }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+
+        protected void BindFundingSourceDropDownList()
+        {
+            FundingSourceDTOCollection fundingSourceCol = LookupDataBL.Instance.GetFundingSource();
+            ddlFundingSource.DataSource = fundingSourceCol;
+            ddlFundingSource.DataTextField = "FundingSourceName";
+            ddlFundingSource.DataValueField = "FundingSourceID";
+            ddlFundingSource.DataBind();
+        }
+        /// <summary>
+        /// Bind search data into gridview
+        /// </summary>
+        protected void BindGrvInvoicePaymentList(DateTime periodStart, DateTime periodEnd)
+        {
+            
+            try
+            {
+                InvoicePaymentDTOCollection invoicePayment = GetInvoicePaymentInfo(periodStart, periodEnd);
+                //bind search data to gridview
+               
+                if (invoicePayment!= null)
+                {
+                    grvInvoicePaymentList.DataSource = invoicePayment;
+                    ViewState["invoicePayment"] = invoicePayment;
+                }
+                else
+                {
+                    grvInvoicePaymentList.SelectedIndex = -1;
+                    invoicePayment = null;
+                }
+                //
+                grvInvoicePaymentList.DataBind();
+                for (int i = 0; i < grvInvoicePaymentList.Rows.Count; i++)
+                {
+                    grvInvoicePaymentList.Rows[i].Attributes.Add("onclick", Page.ClientScript.GetPostBackEventReference(grvInvoicePaymentList, "Select$" + i));
+                }
+            }
+            catch (Exception ex)
+            {
+                lblMessage.Text = ex.Message;
+                ExceptionProcessor.HandleException(ex);
+            }
+        }
+        protected InvoicePaymentDTOCollection GetInvoicePaymentInfo(DateTime periodStart, DateTime periodEnd)
+        {
+            InvoiceSearchCriteriaDTO searchCriteria = new InvoiceSearchCriteriaDTO();
+            InvoicePaymentDTOCollection invoicePayment = new InvoicePaymentDTOCollection();
+            try
+            {
+                //get search criteria to AgencyPayableSearchCriteriaDTO
+                searchCriteria.FundingSourceId = int.Parse(ddlFundingSource.SelectedValue);
+                searchCriteria.PeriodStart = periodStart;
+                searchCriteria.PeriodEnd = periodEnd;
+                //get search data match that search collection
+                invoicePayment = InvoicePaymentBL.Instance.InvoicePaymentSearch(searchCriteria);
+            }
+            catch (Exception ex)
+            {
+                lblMessage.Text = ex.Message;
+                ExceptionProcessor.HandleException(ex);
+            }
+            return invoicePayment;
+        }
+        /// <summary>
+        /// get default period start:1st\prior month\year
+        /// get default period end: lastday\prior month\year
+        /// </summary>
+        protected void SetDefaultPeriodStartEnd()
+        {
+            DateTime today = DateTime.Today;
+            int priormonth = today.AddMonths(-1).Month;
+            int year = today.AddMonths(-1).Year;
+            txtPeriodStart.Text = priormonth + "/" + 1 + "/" + year;
+            int daysinmonth = DateTime.DaysInMonth(year, priormonth);
+            txtPeriodEnd.Text = priormonth + "/" + daysinmonth + "/" + year;
+        }
+
+        protected void btnRefreshList_Click(object sender, EventArgs e)
+        {
+            lblMessage.Text = "";
+            DateTime periodStart, periodEnd;
+            periodStart = DateTime.Parse(txtPeriodStart.Text);
+            periodEnd = DateTime.Parse(txtPeriodEnd.Text);
+            BindGrvInvoicePaymentList(periodStart, periodEnd);
+        }
+       
+        protected void grvInvoiceList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            InvoicePaymentDTOCollection invoicePayment = new InvoicePaymentDTOCollection();
+            invoicePayment = (InvoicePaymentDTOCollection)ViewState["invoicePayment"];
+            if (invoicePayment != null)
+            {
+                
+                Session["invoicePaymentInfo"] = invoicePayment[grvInvoicePaymentList.SelectedIndex];
+            }
+        }
+
+        protected void btnViewPayable_Click(object sender, EventArgs e)
+        {
+            if (grvInvoicePaymentList.SelectedIndex != -1)
+            {
+                btnViewEditPayable.Attributes.Clear();
+                Response.Redirect("ViewEditInvoicePayment.aspx");
+            }
+            else
+                btnViewEditPayable.Attributes.Add("onclick", "alert('You have to choose a row in gridview!')");
+
+        }
+    }
+}
