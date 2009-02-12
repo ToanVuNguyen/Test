185830ab57bdc1c6adaebb5ce672d0da62eb15eb
t55nguye <t55nguye@6d9d970a-e046-46de-96ce-fbc9f6c46dd9>
2009-02-12 09:27:56 +0000

    Webpage name change
    git-svn-id: http://nt-svn-jira.vdc.csc.com/HPF/Implementation/Source Code@1605 6d9d970a-e046-46de-96ce-fbc9f6c46dd9

--- HPF.FutureState/HPF.FutureState.Common/DataTransferObjects/AgencyPayableCaseDTO.cs
+++ HPF.FutureState/HPF.FutureState.Common/DataTransferObjects/AgencyPayableCaseDTO.cs
@@ -12,12 +12,21 @@ namespace HPF.FutureState.Common.DataTransferObjects
         public int? ForeclosureCaseId { get; set; }
         public DateTime? PeriodStartDate { get; set; }
         public DateTime? PeriodEndDate { get; set; }
-        public decimal? PaymentAmount { get; set; }
+        public double? PaymentAmount { get; set; }
         public DateTime? PaymentDate { get; set; }
         public int? AgencyPayableId { get; set; }
         public string NFMCDifferenceEligibleInd {get;set;}
 	    public DateTime? TakebackPmtIdentifiedDt {get;set;}
 	    public string TakebackPmtReasonCd {get;set;}
         public double? NFMCDifferencePaidAmt { get; set; }
+        //---------view/edit agency payable
+        public int? AgencyCaseID { get; set; }
+        public DateTime? CreateDt { get; set; }
+        public DateTime? CompleteDt { get; set; }
+        public string LoanNum { get; set; }
+        public string ServicerName { get; set; }
+        public string BorrowerName { get; set; }
+        public string TakebackReason { get; set; }
+        public DateTime? TakebackDate { get; set; }
     }
 }
