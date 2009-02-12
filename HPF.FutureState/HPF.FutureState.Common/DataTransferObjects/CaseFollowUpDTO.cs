0311b69b3e575251a2d5fcdb3a9ece4027f87adc
kdo <kdo@6d9d970a-e046-46de-96ce-fbc9f6c46dd9>
2009-02-12 04:56:10 +0000

    CaseFollowUpDTO,CaseFollowUpDTOCollection,CaseFollowUpDAO, CaseFollowUpUnitTest
    git-svn-id: http://nt-svn-jira.vdc.csc.com/HPF/Implementation/Source Code@1587 6d9d970a-e046-46de-96ce-fbc9f6c46dd9

---
+++ HPF.FutureState/HPF.FutureState.Common/DataTransferObjects/CaseFollowUpDTO.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+
+namespace HPF.FutureState.Common.DataTransferObjects
+{
+    public class CaseFollowUpDTO : BaseDTO
+    {
+        public int? FcId { get; set; }
+
+        public int? CasePostCounselingStatusId { get; set; }
+
+        public int? OutcomeTypeId { get; set; } 
+
+        public DateTime? FollowUpDt { get; set; }
+
+        public string FollowUpComment { get; set; }
+
+        public string FollowupSourceCd { get; set; }
+
+        public string LoanDelinqStatusCd { get; set; }
+
+        public string StillInHouseInd { get; set; }
+
+        public string CreditScore { get; set; }
+
+        public string CreditBureauCd { get; set; }
+
+        public DateTime? CreditReportDt { get; set; }        
+    }
+}
---
+++ HPF.FutureState/HPF.FutureState.DataAccess/CaseFollowUpDAO.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+
+using HPF.FutureState.Common.DataTransferObjects;
+using HPF.FutureState.Common.Utils.Exceptions;
+using System.Data;
+
+namespace HPF.FutureState.DataAccess
+{
+    public class CaseFollowUpDAO : BaseDAO
+    {
+        private static readonly CaseFollowUpDAO instance = new CaseFollowUpDAO();
+        /// <summary>
+        /// Singleton
+        /// </summary>
+        public static CaseFollowUpDAO Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+        protected CaseFollowUpDAO()
+        {
+        }
+
+        public bool SaveCaseFollowUp(CaseFollowUpDTO caseFollowUp, bool isUpdated)
+        {
+            bool bReturn = false;
+            var dbConnection=CreateConnection();
+            var command = CreateSPCommand("hpf_case_post_counseling_status_update", dbConnection);
+            if (!isUpdated)
+            {
+                command = CreateSPCommand("hpf_case_post_counseling_status_insert", dbConnection);
+                command.Parameters.Add(new SqlParameter("@po_case_post_counseling_status_id", SqlDbType.Int) { Direction = ParameterDirection.Output });
+                command.Parameters.Add(new SqlParameter("@pi_create_dt", caseFollowUp.CreateDate));
+                command.Parameters.Add(new SqlParameter("@pi_create_user_id", caseFollowUp.CreateUserId));
+                command.Parameters.Add(new SqlParameter("@pi_create_app_name", caseFollowUp.CreateAppName));
+            }
+            if (isUpdated)
+            {
+                command.Parameters.Add(new SqlParameter("@pi_case_post_counseling_status_id", caseFollowUp.CasePostCounselingStatusId));
+            }
+            command.Parameters.Add(new SqlParameter("@pi_fc_id", caseFollowUp.FcId));
+            command.Parameters.Add(new SqlParameter("@pi_followup_dt", caseFollowUp.FollowUpDt));
+            command.Parameters.Add(new SqlParameter("@pi_followup_comment", caseFollowUp.FollowUpComment));
+            command.Parameters.Add(new SqlParameter("@pi_followup_source_cd", caseFollowUp.FollowupSourceCd));
+            command.Parameters.Add(new SqlParameter("@pi_loan_delinq_status_cd", caseFollowUp.LoanDelinqStatusCd));
+            command.Parameters.Add(new SqlParameter("@pi_still_in_house_ind", caseFollowUp.StillInHouseInd));
+            command.Parameters.Add(new SqlParameter("@pi_credit_score", caseFollowUp.CreditScore));
+            command.Parameters.Add(new SqlParameter("@pi_credit_bureau_cd", caseFollowUp.CreditBureauCd));
+            command.Parameters.Add(new SqlParameter("@pi_credit_report_dt", caseFollowUp.CreditReportDt));
+            command.Parameters.Add(new SqlParameter("@pi_outcome_type_id", caseFollowUp.OutcomeTypeId));
+            command.Parameters.Add(new SqlParameter("@pi_chg_lst_dt", caseFollowUp.ChangeLastDate));
+            command.Parameters.Add(new SqlParameter("@pi_chg_lst_user_id", caseFollowUp.ChangeLastUserId));
+            command.Parameters.Add(new SqlParameter("@pi_chg_lst_app_name", caseFollowUp.ChangeLastAppName));
+            
+            try
+            {
+                dbConnection.Open();
+                command.ExecuteNonQuery();
+                bReturn = true;
+            }
+            catch (Exception Ex)
+            {
+                bReturn = false;
+                throw ExceptionProcessor.Wrap<DataAccessException>(Ex);                
+            }
+            finally
+            {
+                dbConnection.Close();
+            }
+            return bReturn;
+        }
+
+        public CaseFollowUpDTOCollection GetFollowUp(int fcId)
+        {
+            CaseFollowUpDTOCollection result = new CaseFollowUpDTOCollection();
+            SqlConnection dbConnection = CreateConnection();
+            SqlCommand command = CreateSPCommand("hpf_case_post_counseling_status_get", dbConnection);
+            //<Parameter>
+            var sqlParam = new SqlParameter[1];
+            sqlParam[0] = new SqlParameter("@pi_fc_id", fcId);            
+            command.Parameters.AddRange(sqlParam);
+            //</Parameter>
+            try
+            {
+                dbConnection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    CaseFollowUpDTO caseFollowUp = new CaseFollowUpDTO();
+
+                    caseFollowUp.FcId = ConvertToInt(reader["fc_id"]);
+                    caseFollowUp.CasePostCounselingStatusId = ConvertToInt(reader["case_post_counseling_status_id"]);
+                    caseFollowUp.FollowUpDt = ConvertToDateTime(reader["followup_dt"]);
+                    caseFollowUp.FollowUpComment = ConvertToString(reader["followup_comment"]);
+                    caseFollowUp.FollowupSourceCd = ConvertToString(reader["followup_source_cd"]);
+                    caseFollowUp.LoanDelinqStatusCd = ConvertToString(reader["loan_delinq_status_cd"]);
+                    caseFollowUp.StillInHouseInd = ConvertToString(reader["still_in_house_ind"]);
+                    caseFollowUp.CreditScore = ConvertToString(reader["credit_score"]);
+                    caseFollowUp.CreditBureauCd = ConvertToString(reader["credit_bureau_cd"]);
+                    caseFollowUp.CreditReportDt = ConvertToDateTime(reader["credit_report_dt"]);
+                    caseFollowUp.OutcomeTypeId = ConvertToInt(reader["outcome_type_id"]);
+
+                    result.Add(caseFollowUp);
+
+                }
+            }
+            catch (Exception Ex)
+            {
+                throw ExceptionProcessor.Wrap<DataAccessException>(Ex);
+            }
+            finally
+            {
+                dbConnection.Close();
+            }
+
+            return result;
+        }
+
+    }
+}
