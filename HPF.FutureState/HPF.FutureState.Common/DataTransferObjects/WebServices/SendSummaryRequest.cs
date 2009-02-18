7365f8f8c1c95e0e4004ecebc4d05a0e0082c670
ctsai5-ext <ctsai5-ext@6d9d970a-e046-46de-96ce-fbc9f6c46dd9>
2009-02-18 21:14:47 +0000

    Implement WS SendSummary
    git-svn-id: http://nt-svn-jira.vdc.csc.com/HPF/Implementation/Source Code@1813 6d9d970a-e046-46de-96ce-fbc9f6c46dd9

---
+++ HPF.FutureState/HPF.FutureState.Common/DataTransferObjects/WebServices/SendSummaryRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HPF.FutureState.Common.DataTransferObjects.WebServices
+{
+    public class SendSummaryRequest : BaseRequest
+    {
+        public string SenderId { get; set; }
+        public string EmailToAddress { get; set; }
+        public string EmailSubject { get; set; }
+        public string EmailBody { get; set; }
+        public int FCId { get; set; }
+    }
+}
