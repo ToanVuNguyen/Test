c60dfb1e8add728688687ebf288350cb558b2e2b
nncao <nncao@6d9d970a-e046-46de-96ce-fbc9f6c46dd9>
2009-02-26 03:36:14 +0000

    git-svn-id: http://nt-svn-jira.vdc.csc.com/HPF/Implementation/Source Code@2140 6d9d970a-e046-46de-96ce-fbc9f6c46dd9

---
+++ HPF.FutureState/HPF.SharePointAPI/BusinessEntity/CounselingSummaryInfo.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HPF.SharePointAPI.Enum;
+
+namespace HPF.SharePointAPI.BusinessEntity
+{
+    public class CounselingSummaryInfo:BaseObject
+    {
+        private string _loanNumber;
+        private string _servicer;
+        private DateTime? _completedDate;
+        private DateTime? _foreclosureSaleDate;
+        private string _delinquency;
+        private ReviewStatus _reviewStatus;
+
+        public string LoanNumber
+        {
+            get { return _loanNumber; }
+            set { _loanNumber = value; }
+        }
+
+        public string Servicer
+        {
+            get { return _servicer; }
+            set { _servicer = value; }
+        }
+
+        public DateTime? CompletedDate
+        {
+            get { return _completedDate; }
+            set { _completedDate = value; }
+        }
+
+        public DateTime? ForeclosureSaleDate
+        {
+            get { return _foreclosureSaleDate; }
+            set { _foreclosureSaleDate = value; }
+        }
+
+        public string Delinquency
+        {
+            get { return _delinquency; }
+            set { _delinquency = value; }
+        }
+
+        public ReviewStatus ReviewStatus
+        {
+            get { return _reviewStatus; }
+            set { _reviewStatus = value; }
+        }
+
+        public CounselingSummaryInfo():base()
+        {
+            _reviewStatus = Enum.ReviewStatus.PendingReview;
+        }
+        public CounselingSummaryInfo(string name, byte[] file,
+             string loanNumber, string servicer, DateTime completedDate, 
+             DateTime foreclosureSaleDate, string delinquency, ReviewStatus reviewStatus):
+            base(name, file)
+        {
+            _loanNumber = loanNumber;
+            _servicer = servicer;
+            _completedDate = completedDate;
+            _foreclosureSaleDate = foreclosureSaleDate;
+            _delinquency = delinquency;
+            _reviewStatus = reviewStatus;
+        }
+    }
+}
