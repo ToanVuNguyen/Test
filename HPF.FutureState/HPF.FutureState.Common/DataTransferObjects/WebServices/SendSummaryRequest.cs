7c27e43c27688782e8280616a1ebe6bea8d8e1fe
ctsai5-ext <ctsai5-ext@6d9d970a-e046-46de-96ce-fbc9f6c46dd9>
2009-03-05 20:15:31 +0000

    added validation for field length
    git-svn-id: http://nt-svn-jira.vdc.csc.com/HPF/Implementation/Source Code@2476 6d9d970a-e046-46de-96ce-fbc9f6c46dd9

--- HPF.FutureState/HPF.FutureState.Common/DataTransferObjects/WebServices/SendSummaryRequest.cs
+++ HPF.FutureState/HPF.FutureState.Common/DataTransferObjects/WebServices/SendSummaryRequest.cs
@@ -2,15 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using HPF.FutureState.Common.Utils.DataValidator;
 
 namespace HPF.FutureState.Common.DataTransferObjects.WebServices
 {
     public class SendSummaryRequest : BaseRequest
     {
+        [NullableOrStringLengthValidator(true, 30, "Sender Id", Ruleset = Constant.RULESET_LENGTH, Tag = ErrorMessages.ERR0809)]
         public string SenderId { get; set; }
+
+        [NullableOrStringLengthValidator(true, 255, "Email To Address", Ruleset = Constant.RULESET_LENGTH, Tag = ErrorMessages.ERR0806)]
         public string EmailToAddress { get; set; }
+
+        [NullableOrStringLengthValidator(true, 255, "Email Subject", Ruleset = Constant.RULESET_LENGTH, Tag = ErrorMessages.ERR0807)]
         public string EmailSubject { get; set; }
+
+        [NullableOrStringLengthValidator(true, 2000, "Email Body", Ruleset = Constant.RULESET_LENGTH, Tag = ErrorMessages.ERR0808)]
         public string EmailBody { get; set; }
+
         public int FCId { get; set; }
     }
 }
