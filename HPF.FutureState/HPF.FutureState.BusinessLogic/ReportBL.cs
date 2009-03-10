50f5eb3e58b98725b36d0cbf32d31b8b859be542
ltcao <ltcao@6d9d970a-e046-46de-96ce-fbc9f6c46dd9>
2009-03-10 02:45:19 +0000

    Add ReportBL
    git-svn-id: http://nt-svn-jira.vdc.csc.com/HPF/Implementation/Source Code@2639 6d9d970a-e046-46de-96ce-fbc9f6c46dd9

---
+++ HPF.FutureState/HPF.FutureState.BusinessLogic/ReportBL.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HPF.FutureState.BusinessLogic
+{
+    public class ReportBL
+    {
+        private static readonly ReportBL instance = new ReportBL();
+        /// <summary>
+        /// Singleton Instance
+        /// </summary>
+        public static ReportBL Instance
+        {
+            get { return instance; }
+        }
+
+        protected ReportBL()
+        {
+            
+        }
+    }
+}
