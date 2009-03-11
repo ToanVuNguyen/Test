127d0b7400312bf7eae43f337d39f843177e5303
t55nguye <t55nguye@6d9d970a-e046-46de-96ce-fbc9f6c46dd9>
2009-03-11 11:34:37 +0000

    fix bug
    git-svn-id: http://nt-svn-jira.vdc.csc.com/HPF/Implementation/Source Code@2706 6d9d970a-e046-46de-96ce-fbc9f6c46dd9

---
+++ HPF.FutureState/HPF.FutureState.Common/Utils/HPFPortalNewAgencyPayable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HPF.FutureState.Common.Utils
+{
+    public class HPFPortalNewAgencyPayable
+    {
+        public byte[] ReportFile { get; set; }
+        public string ReportFileName { get; set; }
+        public string FundingSource { get; set;}
+        public string InvoiceNumber { get; set; }
+        public DateTime? Date { get; set; }
+    }
+}
