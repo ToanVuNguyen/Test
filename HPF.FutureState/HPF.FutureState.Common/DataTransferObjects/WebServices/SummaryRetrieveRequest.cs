93f195338c115eeebc0b2f0f363b774518c0f739
pdang <pdang@6d9d970a-e046-46de-96ce-fbc9f6c46dd9>
2009-05-18 09:22:47 +0000

    Implement RetrieveSummary
    git-svn-id: http://nt-svn-jira.vdc.csc.com/HPF/Implementation/Source Code@3839 6d9d970a-e046-46de-96ce-fbc9f6c46dd9

---
+++ HPF.FutureState/HPF.FutureState.Common/DataTransferObjects/WebServices/SummaryRetrieveRequest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HPF.FutureState.Common.DataTransferObjects.WebServices
+{    
+    public class SummaryRetrieveRequest: BaseRequest
+    {
+        /// <summary>
+        /// Foreclosure case id
+        /// </summary>
+        public int? ForeclosureId { get; set; }
+        /// <summary>
+        /// If ReportOutput= None then output is ForeclosureSet
+        /// Otherwise the output is report summary buffer
+        /// </summary>
+        public string ReportOutput { get; set; }
+    }
+}
