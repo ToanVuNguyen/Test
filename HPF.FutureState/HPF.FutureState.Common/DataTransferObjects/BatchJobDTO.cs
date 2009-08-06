377aa11d44e430a2d3bf78afc9f83f4b367dd51f
pdang <pdang@6d9d970a-e046-46de-96ce-fbc9f6c46dd9>
2009-08-06 08:21:22 +0000

    Implement #BUG-414
    git-svn-id: http://nt-svn-jira.vdc.csc.com/HPF/Implementation/Source Code@4102 6d9d970a-e046-46de-96ce-fbc9f6c46dd9

---
+++ HPF.FutureState/HPF.FutureState.Common/DataTransferObjects/BatchJobDTO.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HPF.FutureState.Common.DataTransferObjects
+{
+    public enum RequestorType{Servicer, FundingSource, Agency, CallCenter};
+    public enum JobFrequency { Daily = 1, Weekly = 7};
+
+    [Serializable]
+    public class BatchJobDTO:BaseDTO
+    {
+        public int BatchJobId { get; set; }
+        public string JobName { get; set; }
+        public string JobDescription { get; set; }
+        public JobFrequency JobFrequency { get; set; }
+        DateTime jobStartDate;
+        public DateTime JobStartDate 
+        {
+            get { return jobStartDate; }
+            set { jobStartDate = new DateTime(value.Year, value.Month, value.Day); }
+        }
+        DateTime jobLastStartDt;
+        public DateTime JobLastStartDt 
+        {
+            get { return jobLastStartDt; }
+            set { jobLastStartDt = new DateTime(value.Year, value.Month, value.Day); }
+        }
+        public RequestorType RequestorType { get; set; }
+        public int RequestorId { get; set; }
+        string outputFormat;
+        public string OutputFormat 
+        {
+            get { return outputFormat; }
+            set { outputFormat = string.IsNullOrEmpty(value) ? null : value.Trim(); }
+        }
+        public string OutputDestination { get; set; }
+    }
+}
---
+++ HPF.FutureState/HPF.FutureState.Common/Utils/HPFPortalFannieMae.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HPF.FutureState.Common.Utils
+{    
+    public class HPFPortalFannieMae
+    {
+        public byte[] ReportFile { get; set; }
+        public string ReportFileName { get; set; }        
+        public string SPFolderName { get; set; }
+        public DateTime? StartDt { get; set; }
+        public DateTime? EndDt { get; set; }
+    }
+}
