a0c3b8001557eb053f35bde489a6059d3fd46cd1
qnguyen <qnguyen@6d9d970a-e046-46de-96ce-fbc9f6c46dd9>
2009-08-06 09:55:53 +0000

    git-svn-id: http://nt-svn-jira.vdc.csc.com/HPF/Implementation/Source Code@4117 6d9d970a-e046-46de-96ce-fbc9f6c46dd9

--- HPF.FutureState/HPF.FutureState.DataAccess/SQLScripts/Install/2_HPF_Table_create.sql
+++ HPF.FutureState/HPF.FutureState.DataAccess/SQLScripts/Install/2_HPF_Table_create.sql
@@ -1065,3 +1065,49 @@ CREATE TABLE [dbo].[admin_task_log](
 	CONSTRAINT PK_admin_task_log PRIMARY KEY CLUSTERED ([admin_task_log_id] ASC)
 )
 GO
+
+CREATE TABLE [dbo].[batch_job](
+	[batch_job_id] [int] IDENTITY(1,1) NOT NULL,
+	[job_name] [varchar](50) NOT NULL,
+	[job_description] [varchar](100)  NULL,
+	[job_frequency] [varchar](15)  NOT NULL,
+	[job_start_dt] [datetime] NOT NULL,
+	[job_last_start_dt] [datetime] NOT NULL,
+	[requestor_type] [varchar](20)  NOT NULL,
+	[requestor_id] [int] NOT NULL,
+	[output_format] [varchar](50)  NOT NULL,
+	[output_destination] [varchar](500)  NULL,
+	[create_dt] [datetime] NOT NULL,
+	[create_user_id] [varchar](30)  NOT NULL,
+	[create_app_name] [varchar](20)  NOT NULL,
+	[chg_lst_dt] [datetime] NOT NULL,
+	[chg_user_id] [varchar](30)  NOT NULL,
+	[chg_app_name] [varchar](20)  NOT NULL,
+ CONSTRAINT [PK_batch_job] PRIMARY KEY CLUSTERED 
+(
+	[batch_job_id] ASC
+)WITH (IGNORE_DUP_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+
+--=================
+CREATE TABLE [dbo].[batch_job_log](
+	[batch_job_log_id] [int] IDENTITY(1,1) NOT NULL,
+	[batch_job_id] [int] NOT NULL,
+	[job_result] [varchar](50)  NOT NULL,
+	[record_count] [int] NOT NULL,
+	[job_notes] [varchar](500)  NOT NULL,
+	[create_dt] [datetime] NOT NULL,
+	[create_user_id] [varchar](30)  NOT NULL,
+	[create_app_name] [varchar](20)  NOT NULL,
+	[chg_lst_dt] [datetime] NOT NULL,
+	[chg_user_id] [varchar](30)  NOT NULL,
+	[chg_app_name] [varchar](20)  NOT NULL,
+ CONSTRAINT [PK_batch_job_log] PRIMARY KEY CLUSTERED 
+(
+	[batch_job_log_id] ASC
+)WITH (IGNORE_DUP_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[batch_job_log]  WITH CHECK ADD  CONSTRAINT [FK_batch_job_log_batch_job] FOREIGN KEY([batch_job_id])
+REFERENCES [dbo].[batch_job] ([batch_job_id])
+GO
