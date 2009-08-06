f23a6639c3663be6d4eefb8d9664ca7eaa3e57ed
pdang <pdang@6d9d970a-e046-46de-96ce-fbc9f6c46dd9>
2009-08-06 08:26:24 +0000

    Implement #BUG-414
    git-svn-id: http://nt-svn-jira.vdc.csc.com/HPF/Implementation/Source Code@4104 6d9d970a-e046-46de-96ce-fbc9f6c46dd9

---
+++ HPF.FutureState/HPF.SharePointAPI/BusinessEntity/FannieMaeInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HPF.SharePointAPI.BusinessEntity
+{
+    public class FannieMaeInfo : BaseObject
+    {
+        public string FileName { get; set; }
+        public DateTime? StartDt { get; set; }
+        public DateTime? EndDt { get; set; }
+    }
+}
