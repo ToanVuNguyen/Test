145933a559954eb02ed50936ed9f98265ed9ec2a
pdang2 <pdang2@6d9d970a-e046-46de-96ce-fbc9f6c46dd9>
2009-08-12 06:32:48 +0000

    Fixed #BUG-410
    git-svn-id: http://nt-svn-jira.vdc.csc.com/HPF/Implementation/Source Code@4145 6d9d970a-e046-46de-96ce-fbc9f6c46dd9

---
+++ HPF.FutureState/HPF.FutureState.Common/DataTransferObjects/SendSummaryServicerCollectionDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HPF.FutureState.Common.DataTransferObjects
+{
+    [Serializable]
+    public class SendSummaryServicerCollectionDTO: BaseDTOCollection<SendSummaryServicerDTO>
+    {
+        public SendSummaryServicerDTO GetServicerById(int? servicerId)
+        {
+            return this.SingleOrDefault(i => i.ServicerID == servicerId);
+        }
+    }
+}
---
+++ HPF.FutureState/HPF.FutureState.Common/DataTransferObjects/SendSummaryServicerDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HPF.FutureState.Common.DataTransferObjects
+{
+    [Serializable]
+    public class SendSummaryServicerDTO: ServicerDTO
+    {
+        public string Description { get; set; }
+        public string SummaryDeliveryMethodDesc { get; set; }
+    }
+}
