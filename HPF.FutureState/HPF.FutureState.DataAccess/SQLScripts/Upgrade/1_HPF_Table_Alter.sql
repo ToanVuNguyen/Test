bce65b6815234a3eea3775fa120e429268143f97
qnguyen26 <qnguyen26@6d9d970a-e046-46de-96ce-fbc9f6c46dd9>
2009-09-21 06:58:23 +0000

    git-svn-id: http://nt-svn-jira.vdc.csc.com/HPF/Implementation/Source Code@4200 6d9d970a-e046-46de-96ce-fbc9f6c46dd9

--- HPF.FutureState/HPF.FutureState.DataAccess/SQLScripts/Upgrade/1_HPF_Table_Alter.sql
+++ HPF.FutureState/HPF.FutureState.DataAccess/SQLScripts/Upgrade/1_HPF_Table_Alter.sql
@@ -1,9 +1,38 @@
 -- =============================================
--- Create date: 27 Aug 2009
+-- Create date: 21 Sep 2009
 -- Project : HPF 
 -- Build 
 -- =============================================
 USE HPF
 GO
 
-INSERT INTO menu_item (menu_item_id, menu_group_id,item_name, item_sort_order, item_target, visibled)   VALUES (21,4,'Code Maintenance',3,'CodeMaintenance.aspx',1);
+SET IDENTITY_INSERT dbo.program ON
+GO
+
+insert into program (program_id, program_name, start_dt, create_dt, create_user_id, create_app_name, chg_lst_dt, chg_lst_user_id, chg_lst_app_name)
+values (2001, 'Escalation', '20090922', getdate(), 'ctomczik', 'SQL', getdate(), 'ctomczik', 'SQL')
+
+SET IDENTITY_INSERT dbo.program OFF
+GO
+
+insert into agency_rate (program_id, agency_id, pmt_rate, eff_dt, nfmc_upcharge_pmt_rate, create_dt, create_user_id, create_app_name, chg_lst_dt, chg_lst_user_id, chg_lst_app_name)
+values (2001, 17081, 120, '20090922', 10, getdate(), 'ctomczik', 'SQL', getdate(), 'ctomczik', 'SQL')
+
+
+-- Create never bill reason codes
+insert into ref_code_item (ref_code_set_name, code, code_desc, code_comment, sort_order, active_ind,
+       create_dt, create_user_id, create_app_name, chg_lst_dt, chg_lst_user_id, chg_lst_app_name)
+values ('never bill reason code', 'DUPEESCP', 'Duplicate escalation of partial', '', 9, 'Y', getdate(), 'ctomczik', 'SQL Studio', getdate(), 'ctomczik', 'SQL Studio')
+
+insert into ref_code_item (ref_code_set_name, code, code_desc, code_comment, sort_order, active_ind,
+       create_dt, create_user_id, create_app_name, chg_lst_dt, chg_lst_user_id, chg_lst_app_name)
+values ('never bill reason code', 'DUPEESCC', 'Duplicate escalation of completed', '', 10, 'Y', getdate(), 'ctomczik', 'SQL Studio', getdate(), 'ctomczik', 'SQL Studio')
+
+-- Create never pay reason codes
+insert into ref_code_item (ref_code_set_name, code, code_desc, code_comment, sort_order, active_ind,
+       create_dt, create_user_id, create_app_name, chg_lst_dt, chg_lst_user_id, chg_lst_app_name)
+values ('never pay reason code', 'DUPEESCP', 'Duplicate escalation of partial', '', 9, 'Y', getdate(), 'ctomczik', 'SQL Studio', getdate(), 'ctomczik', 'SQL Studio')
+
+insert into ref_code_item (ref_code_set_name, code, code_desc, code_comment, sort_order, active_ind,
+       create_dt, create_user_id, create_app_name, chg_lst_dt, chg_lst_user_id, chg_lst_app_name)
+values ('never pay reason code', 'DUPEESCC', 'Duplicate escalation of completed', '', 10, 'Y', getdate(), 'ctomczik', 'SQL Studio', getdate(), 'ctomczik', 'SQL Studio')
