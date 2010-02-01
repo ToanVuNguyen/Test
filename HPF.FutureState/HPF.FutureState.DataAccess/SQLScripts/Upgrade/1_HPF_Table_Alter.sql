9b88df1093d1410a51639c1ebc97cd1720cef5d8
qnguyen26 <qnguyen26@6d9d970a-e046-46de-96ce-fbc9f6c46dd9>
2010-02-01 08:00:22 +0000

    git-svn-id: http://nt-svn-jira.vdc.csc.com/HPF/Implementation/Source Code@4367 6d9d970a-e046-46de-96ce-fbc9f6c46dd9

--- HPF.FutureState/HPF.FutureState.DataAccess/SQLScripts/Upgrade/1_HPF_Table_Alter.sql
+++ HPF.FutureState/HPF.FutureState.DataAccess/SQLScripts/Upgrade/1_HPF_Table_Alter.sql
@@ -1,5 +1,5 @@
 -- =============================================
--- Create date: 19 Jan 2010
+-- Create date: 1 Feb 2010
 -- Project : HPF 
 -- Build 
 -- Description:	
@@ -7,42 +7,7 @@
 USE HPF
 GO
 
-CREATE TABLE mha_help
-(	borrower_fname		varchar(30)
-	,borrower_lname		varchar(30)
-	,servicer			varchar(50)
-	,servicer_id		int
-	,acct_num			varchar(30)
-	,counselor_name		varchar(60)
-	,counselor_email	varchar(50)
-	,call_src			varchar(50)
-	,voicemail_dt		datetime
-	,mha_help_reason	varchar(100)
-	,comments			varchar(3000)
-	,privacy_consent	varchar(10)
-	,borrower_in_trial_mod	varchar(10)
-	,trial_mod_before_sept1	varchar(10)
-	,current_on_payments	varchar(10)
-	,wage_earner		varchar(10)
-	,two_paystubs_sent	varchar(10)
-	,all_docs_submitted	varchar(10)
-	,list_of_docs_submitted	varchar(500)
-	,borrower_phone		varchar(50)
-	,best_time_to_reach	varchar(30)
-	,borrower_email		varchar(60)
-	,address			varchar(3000)
-	,city				varchar(255)
-	,state				varchar(2)
-	,zip				varchar(5)
-	,mha_help_resolution	varchar(200)
-	,item_created_dt	datetime
-	,item_created_user	varchar(50)
-	,item_modified_dt	datetime
-	,item_modified_user	varchar(50)
-	,created_dt			datetime
-	,created_user_id		varchar(30)
-	,created_app_name	varchar(20)
-	,chg_lst_dt			datetime
-	,chg_lst_user_id	varchar(30)
-	,chg_lst_app_name	varchar(20)
-);
+ALTER TABLE  mha_escalation ADD	item_created_user varchar(50) NULL;
+ALTER TABLE  mha_escalation ADD item_modified_user varchar(50) NULL;
+ALTER TABLE  mha_escalation ADD item_modified_dt datetime NULL;
+EXEC sp_rename 'mha_escalation.created_dt', 'item_created_dt', 'COLUMN';
