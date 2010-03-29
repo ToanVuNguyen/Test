5ade6fb5c33ff0cab642dc270949e1114070f999
pdang2 <pdang2@6d9d970a-e046-46de-96ce-fbc9f6c46dd9>
2010-03-29 07:06:45 +0000

    Implement 498
    git-svn-id: http://nt-svn-jira.vdc.csc.com/HPF/Implementation/Source Code@4445 6d9d970a-e046-46de-96ce-fbc9f6c46dd9

---
+++ HPF.FutureState/HPF.SharePointAPI/BusinessEntity/CounselingSummaryAuditLogInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HPF.SharePointAPI.BusinessEntity
+{
+    public class CounselingSummaryAuditLogInfo: BaseObject
+    {
+        public string CounselingSummaryFile { get; set; }
+        public DateTime? OccurredDate { get; set; }
+        public string Servicer { get; set; }
+        public string LoanNumber { get; set; }
+        public DateTime? CompletedDate { get; set; }
+        public DateTime? ItemCreatedDate { get; set; }
+        public string UserId { get; set; }
+        public string ArchiveName { get; set; }
+    }
+}
