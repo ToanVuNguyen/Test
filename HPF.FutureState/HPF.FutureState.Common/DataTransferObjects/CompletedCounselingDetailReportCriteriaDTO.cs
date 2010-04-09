be013f75d5d252415ce6183703b3aaa932e8c2a9
pdang2 <pdang2@6d9d970a-e046-46de-96ce-fbc9f6c46dd9>
2010-04-09 09:35:06 +0000

    Implement 504
    git-svn-id: http://nt-svn-jira.vdc.csc.com/HPF/Implementation/Source Code@4493 6d9d970a-e046-46de-96ce-fbc9f6c46dd9

---
+++ HPF.FutureState/HPF.FutureState.Common/DataTransferObjects/CompletedCounselingDetailReportCriteriaDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HPF.FutureState.Common.DataTransferObjects
+{
+    [Serializable]
+    public class CompletedCounselingDetailReportCriteriaDTO: BaseDTO
+    {
+        public int AgencyId { get; set; }
+        public int ProgramId { get; set; }
+        public DateTime FromDate { get; set; }
+        public DateTime ToDate { get; set; }
+    }
+}
---
+++ HPF.FutureState/HPF.SharePointAPI/BusinessEntity/CompletedCounselingDetailInfo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HPF.SharePointAPI.BusinessEntity
+{
+    public class CompletedCounselingDetailInfo: BaseObject
+    {
+        public DateTime FromDate { get; set; }
+        public DateTime ToDate { get; set; }
+    }
+}
