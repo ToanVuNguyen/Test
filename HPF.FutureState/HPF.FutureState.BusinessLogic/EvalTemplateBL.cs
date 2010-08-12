ee160acb303fabf8166132a5aaf39b48691eee87
tnguyen276 <tnguyen276@6d9d970a-e046-46de-96ce-fbc9f6c46dd9>
2010-08-12 02:32:42 +0000

    Implement for QC Enhancement: Add EvalTemplateBL.cs
    git-svn-id: http://nt-svn-jira.vdc.csc.com/HPF/Implementation/Source Code@4671 6d9d970a-e046-46de-96ce-fbc9f6c46dd9

---
+++ HPF.FutureState/HPF.FutureState.BusinessLogic/EvalTemplateBL.cs
@@ -0,0 +1,26 @@
+﻿using HPF.FutureState.BusinessLogic;
+using HPF.FutureState.Common.DataTransferObjects;
+using HPF.FutureState.DataAccess;
+using System.Xml;
+
+namespace HPF.FutureState.BusinessLogic
+{
+    public class EvalTemplateBL:BaseBusinessLogic
+    {
+        private static readonly EvalTemplateBL instance = new EvalTemplateBL();
+        /// <summary>
+        /// Singleton
+        /// </summary>
+        public static EvalTemplateBL Instance
+        {
+            get { return instance; }
+        }
+        protected EvalTemplateBL()
+        {
+        }
+        public EvalTemplateDTO RetriveTemplate(int? templateId)
+        {
+            return EvalTemplateDAO.Instance.GetEvalTemplateById(templateId);
+        }
+    }
+}
