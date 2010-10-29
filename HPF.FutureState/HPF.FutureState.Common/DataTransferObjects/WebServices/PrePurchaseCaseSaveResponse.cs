614a191fc38cf9411f99456c7782a66928e9c33e
tnguyen276 <tnguyen276@6d9d970a-e046-46de-96ce-fbc9f6c46dd9>
2010-10-29 04:41:33 +0000

    Pre Purchase Counseling: Add DTO classes
    git-svn-id: http://nt-svn-jira.vdc.csc.com/HPF/Implementation/Source Code@4791 6d9d970a-e046-46de-96ce-fbc9f6c46dd9

---
+++ HPF.FutureState/HPF.FutureState.Common/DataTransferObjects/WebServices/PrePurchaseCaseSaveResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HPF.FutureState.Common.DataTransferObjects.WebServices
+{
+    public class PrePurchaseCaseSaveResponse:BaseResponse
+    {
+        public int? PPCaseId { get; set; }
+        public int? PPBorrowerId { get; set; }
+        public DateTime UploadDt { get; set; }
+    }
+}
