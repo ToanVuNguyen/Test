359ae018047063a89fec9d4e3103ab128ed3068d
tnguyen276 <tnguyen276@6d9d970a-e046-46de-96ce-fbc9f6c46dd9>
2011-01-12 06:25:50 +0000

    Handle QC Upload Media Files
    git-svn-id: http://nt-svn-jira.vdc.csc.com/HPF/Implementation/Source Code@4869 6d9d970a-e046-46de-96ce-fbc9f6c46dd9

---
+++ HPF.FutureState/HPF.FutureState.Web/Security/HPFMediaFileHandler.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Web.Services.Protocols;
+using System.Xml.Linq;
+using System.IO;
+
+namespace HPF.FutureState.Web.Security
+{
+    public class HPFMediaFileHandler : IHttpHandler
+    {
+        public void ProcessRequest(HttpContext context)
+        {
+            string filePath = context.Server.MapPath(context.Request.ServerVariables["SCRIPT_NAME"].ToString());
+            FileInfo file = new System.IO.FileInfo(filePath);
+            if (file.Exists)
+            {
+                //return the file
+                context.Response.Clear();
+                context.Response.AddHeader("Content-Disposition", "attachment; filename=" + file.Name);
+                context.Response.AddHeader("Content-Length", file.Length.ToString());
+                context.Response.ContentType = "application/octet-stream";
+                context.Response.WriteFile(file.FullName);
+                context.ApplicationInstance.CompleteRequest();
+                context.Response.End();
+            }
+
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
